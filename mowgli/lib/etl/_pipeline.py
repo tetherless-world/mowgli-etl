e652007e1a7984807f39184851e181bca5b30c28
Minor Gordon <gordom6@rpi.edu>
2020-01-24 11:51:40 -0500

    new cut of _Pipeline class

--- mowgli/lib/etl/_pipeline.py
+++ mowgli/lib/etl/_pipeline.py
@@ -1,10 +1,33 @@
 from abc import ABC
 
+from configargparse import ArgParser
+
 from mowgli.lib.etl._extractor import _Extractor
 from mowgli.lib.etl._transformer import _Transformer
 
 
 class _Pipeline(ABC):
-    def __init__(self, *, extractor: _Extractor, transformer: _Transformer):
-        self.__extractor = extractor
-        self.__transformer = transformer
+    """
+    Construct an extract-transform pipeline.
+    :param extractor: extractor implementation
+    :param id: unique identifier for this pipeline instance, may be adapted from arguments
+    :param transformer: transformer implementation
+    """
+
+    @classmethod
+    def add_arguments(cls, arg_parser: ArgParser) -> None:
+        """
+        Add pipeline-specific arguments. The parsed arguments are passed to the constructor as keywords.
+        """
+
+    @property
+    def extractor(self):
+        return self.__extractor
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def transformer(self):
+        return self.__transformer
