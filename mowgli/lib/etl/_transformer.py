b37f94e82e9d50eece6341701933b4616042a791
Minor Gordon <gordom6@rpi.edu>
2020-01-24 11:47:18 -0500

    _Transformer class

--- mowgli/lib/etl/_transformer.py
+++ mowgli/lib/etl/_transformer.py
@@ -7,5 +7,9 @@ from mowgli.lib.cskg.node import Node
 
 class _Transformer(ABC):
     @abstractmethod
-    def transform(self) -> Generator[Union[Node, Edge], None, None]:
-        pass
+    def transform(self, **kwds) -> Generator[Union[Node, Edge], None, None]:
+        """
+        Transform previously-extracted data.
+        :param kwds: merged dictionary of initial extract kwds and the result of extract
+        :return: generator of nodes and edges
+        """
