0d95599559d510bd652d5a245fedda638a83ce19
Jody Sunray <sunraj@rpi.edu>
2020-01-27 16:51:35 -0500

    eat_transformer function

--- mowgli/lib/etl/eat/eat_transformer.py
+++ mowgli/lib/etl/eat/eat_transformer.py
@@ -5,4 +5,23 @@ class EatTransformer(_Transformer):
     def transform(self, xml_file_path: str):
         self._logger.info("transform %s", xml_file_path)
         # Yield Nodes and Edges
+
+        dom = parse(xml_file_path)
+
+        stimuli = dom.getElementsByTagName('stimulus')
+
+        for stimulus in stimuli:
+            stim_word = str(stimulus.attributes['word'].value)
+            stim_node = Node(datasource="eat", id="eat:" + stim_word, label=stim_word)
+            yield stim_node
+
+            responses = stimulus.getElementsByTagName('response')
+
+            for response in responses:
+                response_word = str(response.attributes['word'].value)
+                response_node = Node(datasource="eat", id="eat:" + response_word, label=response_word)
+                yield response_node
+
+        yield Edge(datasource="eat", object=stim_node, relation="cn:RelatedTo", subject=response_node)
+
         raise NotImplementedError
