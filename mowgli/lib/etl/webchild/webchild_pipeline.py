4e1f213e6ba7f6550ce3ef6e5b98492eb0d37c94
Dhruv Rohatgi <rohatd@rpi.edu>
2020-03-02 12:17:31 -0500

    get rid of backslahes in filepaths in pipeline

--- mowgli/lib/etl/webchild/webchild_pipeline.py
+++ mowgli/lib/etl/webchild/webchild_pipeline.py
@@ -18,10 +18,10 @@ class WebchildPipeline(_Pipeline):
     @classmethod
     def add_arguments(cls, arg_parser):
         path_dir = pathlib.Path(__file__).parent.absolute().parents[3]
-        arg_parser.add_argument("--memberof-csv-file-path", help="WebChild memberof .csv file path", required=False, default =path_dir.joinpath('data\webchild\webchild_partof_memberof.txt'))
-        arg_parser.add_argument("--physical-csv-file-path", help="WebChild physical .csv file path", required=False, default = path_dir.joinpath('data\webchild\webchild_partof_physical.txt'))
-        arg_parser.add_argument("--substanceof-csv-file-path", help="WebChild substance of .csv file path", required=False, default = path_dir.joinpath('data/webchild\webchild_partof_substanceof.txt'))
-        arg_parser.add_argument("--wordnet-csv-file-path", help="WebChild WordNet .csv file path", required=False, default = path_dir.joinpath('data\webchild\WordNetWrapper.txt'))
+        arg_parser.add_argument("--memberof-csv-file-path", help="WebChild memberof .csv file path", required=False, default =path_dir.joinpath('data').joinpath('webchild').joinpath('webchild_partof_memberof.txt'))
+        arg_parser.add_argument("--physical-csv-file-path", help="WebChild physical .csv file path", required=False, default = path_dir.joinpath('data').joinpath('webchild').joinpath('webchild_partof_physical.txt'))
+        arg_parser.add_argument("--substanceof-csv-file-path", help="WebChild substance of .csv file path", required=False, default = path_dir.joinpath('data').joinpath('webchild').joinpath('webchild_partof_substanceof.txt'))
+        arg_parser.add_argument("--wordnet-csv-file-path", help="WebChild WordNet .csv file path", required=False, default = path_dir.joinpath('data').joinpath('webchild').joinpath('WordNetWrapper.txt'))
 
 
 
