6edd0bdeb69a872df53a9a21bf27dbc3cc4e17f4
Dhruv Rohatgi <rohatd@rpi.edu>
2020-03-03 10:45:33 -0500

    added zip extraction functionality in extractor
    unzips physical.zip if neccessary

--- mowgli/lib/etl/webchild/webchild_extractor.py
+++ mowgli/lib/etl/webchild/webchild_extractor.py
@@ -1,4 +1,7 @@
 from mowgli.lib.etl._extractor import _Extractor
+import zipfile
+import pathlib
+from os import path
 
 class WebchildExtractor(_Extractor):
     def __init__(self, *, memberof_csv_file_path: str, physical_csv_file_path: str, substanceof_csv_file_path: str, wordnet_csv_file_path: str, **kwds):
@@ -10,5 +13,17 @@ class WebchildExtractor(_Extractor):
 
     def extract(self, **kwds):
         self._logger.info("extract")
+
+        destination = pathlib.Path(__file__).parent.absolute().parents[3].joinpath('data').joinpath('webchild')
+
+        if (path.exists(str(self.__physical_csv_file_path)[:-3] + 'txt')):
+            self._logger.info("zip already extracted")
+        else:
+            with zipfile.ZipFile(self.__physical_csv_file_path) as zf:
+                self._logger.info("extracting zip")
+                zf.extractall(destination)
+                self._logger.info("zip extracted")
+                
+        self.__physical_csv_file_path = str(self.__physical_csv_file_path)[:-3] + 'txt'
         return {"memberof_csv_file_path": self.__memberof_csv_file_path, "physical_csv_file_path": self.__physical_csv_file_path, "substanceof_csv_file_path" : self.__substanceof_csv_file_path, "wordnet_csv_file_path": self.__wordnet_csv_file_path}
 
