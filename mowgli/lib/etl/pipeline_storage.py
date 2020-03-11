ae689f018b9c1fd503f9f661e3d1600a5b2e3f70
Minor Gordon <gordom6@rpi.edu>
2020-03-11 12:48:56 -0400

    PipelineStorage: don't use transformed_data_dir_path currently; create other directories on demand

--- mowgli/lib/etl/pipeline_storage.py
+++ mowgli/lib/etl/pipeline_storage.py
@@ -6,9 +6,11 @@ class PipelineStorage:
     def __init__(self, *, pipeline_id: str, root_data_dir_path: Path):
         self.__root_data_dir_path = root_data_dir_path
         pipeline_data_dir_path = root_data_dir_path / Path(pipeline_id)
-        self.__extracted_data_dir_path = self.__makedirs(pipeline_data_dir_path / "extracted")
-        self.__loaded_data_dir_path = self.__makedirs(pipeline_data_dir_path / "loaded")
-        self.__transformed_data_dir_path = self.__makedirs(pipeline_data_dir_path / "transformed")
+        self.__extracted_data_dir_path = pipeline_data_dir_path / "extracted"
+        self.__extracted_data_dir_path_exists = False
+        self.__loaded_data_dir_path = pipeline_data_dir_path / "loaded"
+        self.__loaded_data_dir_path_exists = False
+        # self.__transformed_data_dir_path = self.__makedirs(pipeline_data_dir_path / "transformed")
 
     def __makedirs(self, dir_path: Path) -> Path:
         if not os.path.isdir(dir_path):
@@ -17,16 +19,22 @@ class PipelineStorage:
 
     @property
     def extracted_data_dir_path(self) -> Path:
+        if not self.__extracted_data_dir_path_exists:
+            self.__makedirs(self.__extracted_data_dir_path)
+            self.__extracted_data_dir_path_exists = True
         return self.__extracted_data_dir_path
 
     @property
     def loaded_data_dir_path(self) -> Path:
+        if not self.__loaded_data_dir_path_exists:
+            self.__makedirs(self.__loaded_data_dir_path)
+            self.__loaded_data_dir_path_exists = True
         return self.__loaded_data_dir_path
 
     @property
     def root_data_dir_path(self) -> Path:
         return self.__root_data_dir_path
 
-    @property
-    def transformed_data_dir_path(self) -> Path:
-        return self.__transformed_data_dir_path
+    # @property
+    # def transformed_data_dir_path(self) -> Path:
+    #     return self.__transformed_data_dir_path
