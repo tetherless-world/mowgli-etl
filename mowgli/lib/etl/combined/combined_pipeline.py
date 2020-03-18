006de9acb4e44db0cc61bc9729a7b77137aca339
Minor Gordon <gordom6@rpi.edu>
2020-03-18 15:39:28 -0400

    add to combined pipeline

--- mowgli/lib/etl/combined/combined_pipeline.py
+++ mowgli/lib/etl/combined/combined_pipeline.py
@@ -1,11 +1,12 @@
 from typing import Tuple, Optional
 
 from mowgli.lib.etl._pipeline import _Pipeline
-from mowgli.lib.etl.cskg.cskg_csv_transformer import CskgCsvTransformer
 from mowgli.lib.etl.combined.combined_pipeline_extractor import CombinedPipelineExtractor
+from mowgli.lib.etl.cskg.cskg_csv_transformer import CskgCsvTransformer
 from mowgli.lib.etl.eat.eat_pipeline import EatPipeline
 from mowgli.lib.etl.swow.swow_pipeline import SwowPipeline
 from mowgli.lib.etl.usf.usf_pipeline import UsfPipeline
+from tests.mowgli_test.lib.etl.has_part.has_part_pipeline import HasPartPipeline
 
 
 class CombinedPipeline(_Pipeline):
@@ -21,6 +22,7 @@ class CombinedPipeline(_Pipeline):
     def __default_pipelines(self) -> Tuple[_Pipeline, ...]:
         return (
             EatPipeline(),
+            HasPartPipeline(),
             SwowPipeline(),
             UsfPipeline()
         )
