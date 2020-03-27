364a213f8fb072b06d3c7247518026d2ab09fee9
Sam Stouffer <samuellstouffer@gmail.com>
2020-03-27 13:48:05 -0400

    test callable input for mock http client

--- tests/mowgli_test/lib/etl/http_client/mock_etl_http_client_test.py
+++ tests/mowgli_test/lib/etl/http_client/mock_etl_http_client_test.py
@@ -1,3 +1,5 @@
+from io import StringIO
+
 import pytest
 
 from tests.mowgli_test.lib.etl.http_client.mock_etl_http_client import MockEtlHttpClient
@@ -8,6 +10,19 @@ def test_mock_http_client():
     test_url = "http://example.com"
     test_content = "this\nis\ntest\ncontent"
     client.add_mock_response(test_url, test_content)
+    url_contents = client.urlopen(test_url)
+    assert url_contents.read() == test_content
+
+
+def test_mock_http_client_callable():
+    client = MockEtlHttpClient()
+    test_url = "http://example.com"
+    test_content = "this\nis\ntest\ncontent"
+
+    def content_producer():
+        return StringIO(test_content)
+
+    client.add_mock_response(test_url, content_producer)
     url_contents = client.urlopen(test_url)
     assert url_contents.read() == test_content
 
