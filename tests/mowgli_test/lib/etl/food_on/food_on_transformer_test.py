e940acf9f5791411537d24700e83da1d968ed849
Minor Gordon <gordom6@rpi.edu>
2020-04-07 12:21:03 -0400

    first cut of FoodOn pipeline

---
+++ tests/mowgli_test/lib/etl/food_on/food_on_transformer_test.py
@@ -0,0 +1,20 @@
+from mowgli.lib.cskg.edge import Edge
+from mowgli.lib.etl.food_on.food_on_extractor import FoodOnExtractor
+from mowgli.lib.etl.food_on.food_on_transformer import FoodOnTransformer
+
+
+def test_food_on_transformer(pipeline_storage):
+    extract_result = FoodOnExtractor().extract(force=False, storage=pipeline_storage)
+    transformer = FoodOnTransformer()
+
+    edges = []
+    nodes_by_id = {}
+    for result in transformer.transform(**extract_result):
+        if isinstance(result, Edge):
+            edges.append(result)
+        else:
+            assert result.id not in nodes_by_id
+            nodes_by_id[result.id] = result
+    for edge in edges:
+        assert edge.subject in nodes_by_id
+        assert edge.object in nodes_by_id
