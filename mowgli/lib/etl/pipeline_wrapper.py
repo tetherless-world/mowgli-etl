2cf840857b9b460756141493228ba08f09ef06dd
Sam Stouffer <samuellstouffer@gmail.com>
2020-04-23 11:18:53 -0400

    Skip duplicate checking when transforming entire cskg

--- mowgli/lib/etl/pipeline_wrapper.py
+++ mowgli/lib/etl/pipeline_wrapper.py
@@ -1,3 +1,4 @@
+import logging
 from typing import Generator, Union, Dict
 
 from mowgli.lib.cskg.edge import Edge
@@ -8,6 +9,7 @@ from mowgli.lib.etl.pipeline_storage import PipelineStorage
 
 class PipelineWrapper:
     def __init__(self, pipeline: _Pipeline, storage: PipelineStorage):
+        self._logger = logging.getLogger(self.__class__.__name__)
         self.__pipeline = pipeline
         self.__storage = storage
 
@@ -15,9 +17,9 @@ class PipelineWrapper:
         extract_kwds = self.__pipeline.extractor.extract(force=force, storage=self.__storage)
         return extract_kwds if extract_kwds is not None else {}
 
-    def extract_transform_load(self, force: bool = False):
+    def extract_transform_load(self, force: bool = False, dupe_check: bool = True):
         extract_kwds = self.extract(force=force)
-        graph_generator = self.transform(force=force, **extract_kwds)
+        graph_generator = self.transform(force=force, dupe_check=dupe_check, **extract_kwds)
         self.load(graph_generator)
 
     @property
@@ -34,40 +36,44 @@ class PipelineWrapper:
                 else:
                     raise ValueError(type(node_or_edge))
 
-    def transform(self, force: bool = False, **extract_kwds) -> Generator[Union[Edge, Node], None, None]:
+    def transform(self, force: bool = False, dupe_check: bool = True, **extract_kwds) -> Generator[Union[Edge, Node], None, None]:
         edges_by_signature = {}
         nodes_by_id = {}
         node_ids_used_by_edges = set()
 
+        if not dupe_check:
+            self._logger.info("Skipping duplicate checking during transform")
+
         for node_or_edge in self.__pipeline.transformer.transform(**extract_kwds):
-            if isinstance(node_or_edge, Node):
-                node = node_or_edge
-                # Node ID's should be unique in the CSKG.
-                existing_node = nodes_by_id.get(node.id)
-                if existing_node is not None:
-                    if existing_node == node:
-                        # Common case: ignore exact duplicate nodes i.e., nodes that are the same in all fields.
-                        # This happens frequently in the word association sources, where the same word can come
-                        # up as a response to multiple cues.
-                        continue
+            if dupe_check:
+                if isinstance(node_or_edge, Node):
+                    node = node_or_edge
+                    # Node ID's should be unique in the CSKG.
+                    existing_node = nodes_by_id.get(node.id)
+                    if existing_node is not None:
+                        if existing_node == node:
+                            # Common case: ignore exact duplicate nodes i.e., nodes that are the same in all fields.
+                            # This happens frequently in the word association sources, where the same word can come
+                            # up as a response to multiple cues.
+                            continue
+                        else:
+                            # Throw an exception if two nodes have the same id but aren't the same in all of their fields
+                            raise ValueError(
+                                "nodes with same id, different contents: original=%s, duplicate=%s" % (existing_node, node))
                     else:
-                        # Throw an exception if two nodes have the same id but aren't the same in all of their fields
-                        raise ValueError(
-                            "nodes with same id, different contents: original=%s, duplicate=%s" % (existing_node, node))
-                else:
-                    nodes_by_id[node.id] = node
-            elif isinstance(node_or_edge, Edge):
-                edge = node_or_edge
-                # Edges should be unique in the CSKG, meaning that the tuple of (subject, predicate, object) should be unique.
-                existing_subject_edges = edges_by_signature.setdefault(edge.subject, {})
-                existing_predicate_edges = existing_subject_edges.setdefault(edge.predicate, {})
-                existing_object_edge = existing_predicate_edges.get(edge.object)
-                if existing_object_edge is not None:
-                    # Don't try to handle the exact duplicate case differently. It should never happen.
-                    raise ValueError("duplicate edge: original=%s, duplicate=%s" % (existing_object_edge, edge))
-                existing_predicate_edges[edge.object] = edge
-                node_ids_used_by_edges.add(edge.subject)
-                node_ids_used_by_edges.add(edge.object)
+                        nodes_by_id[node.id] = node
+                elif isinstance(node_or_edge, Edge):
+                    edge = node_or_edge
+                    # Edges should be unique in the CSKG, meaning that the tuple of (subject, predicate, object) should be unique.
+                    existing_subject_edges = edges_by_signature.setdefault(edge.subject, {})
+                    existing_predicate_edges = existing_subject_edges.setdefault(edge.predicate, {})
+                    existing_object_edge = existing_predicate_edges.get(edge.object)
+                    if existing_object_edge is not None:
+                        # Don't try to handle the exact duplicate case differently. It should never happen.
+                        raise ValueError("duplicate edge: original=%s, duplicate=%s" % (existing_object_edge, edge))
+                    existing_predicate_edges[edge.object] = edge
+                    node_ids_used_by_edges.add(edge.subject)
+                    node_ids_used_by_edges.add(edge.object)
             yield node_or_edge
         for node_id in nodes_by_id.keys():
             if node_id not in node_ids_used_by_edges:
