25bb449fb02aeef3ac34dd2d955cc274f47e72b9
Minor Gordon <gordom6@rpi.edu>
2020-04-30 12:43:47 -0400

    fix new tests

--- tests/mowgli_test/lib/storage/test_persistent_edge_set.py
+++ tests/mowgli_test/lib/storage/test_persistent_edge_set.py
@@ -1,41 +1,30 @@
 from mowgli.lib.cskg.edge import Edge
 
 try:
-    from mowgli.lib.storage.edge_set import PersistentEdgeSet
+    from mowgli.lib.storage.persistent_edge_set import PersistentEdgeSet
 except ImportError:
     PersistentEdgeSet = None
 
-
-def test_construction(tmpdir):
-    if PersistentEdgeSet is None:
-        return
-
-    edge_set = PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True)
-    with edge_set as edge_set:
-        pass
-    assert edge_set.closed
+if PersistentEdgeSet is not None:
+    def test_construction(tmpdir):
+        edge_set = PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True)
+        with edge_set as edge_set:
+            pass
+        assert edge_set.closed
 
 
-def test_add(edge: Edge, tmpdir):
-    if PersistentEdgeSet is None:
-        return
-
-    with PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as edge_set:
-        edge_set.add(edge)
+    def test_add(edge: Edge, tmpdir):
+        with PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as edge_set:
+            edge_set.add(edge)
 
 
-def test_get_extant(edge: Edge, tmpdir):
-    if PersistentEdgeSet is None:
-        return
-
-    with PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as edge_set:
-        edge_set.add(edge)
-        assert edge_set.get(object_=edge.object, predicate=edge.predicate, subject=edge.subject) == edge
+    def test_get_extant(edge: Edge, tmpdir):
+        with PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as edge_set:
+            edge_set.add(edge)
+            assert edge_set.get(object_=edge.object, predicate=edge.predicate, subject=edge.subject) == edge
 
 
-def test_get_nonextant(edge: Edge, tmpdir):
-    if PersistentEdgeSet is None:
-        return
-    assert False
-    with PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as edge_set:
-        assert edge_set.get(object_=edge.object, predicate=edge.predicate, subject=edge.subject) is None
+    def test_get_nonextant(edge: Edge, tmpdir):
+        assert False
+        with PersistentEdgeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as edge_set:
+            assert edge_set.get(object_=edge.object, predicate=edge.predicate, subject=edge.subject) is None
--- tests/mowgli_test/lib/storage/test_persistent_node_set.py
+++ tests/mowgli_test/lib/storage/test_persistent_node_set.py
@@ -6,36 +6,25 @@ except ImportError:
     PersistentNodeSet = None
 
 
-def test_construction(tmpdir):
-    if PersistentNodeSet is None:
-        return
-
-    node_set = PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True)
-    with node_set as node_set:
-        pass
-    assert node_set.closed
+if PersistentNodeSet is not None:
+    def test_construction(tmpdir):
+        node_set = PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True)
+        with node_set as node_set:
+            pass
+        assert node_set.closed
 
 
-def test_add(node: Node, tmpdir):
-    if PersistentNodeSet is None:
-        return
-
-    with PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as node_set:
-        node_set.add(node)
+    def test_add(node: Node, tmpdir):
+        with PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as node_set:
+            node_set.add(node)
 
 
-def test_get_extant(node: Node, tmpdir):
-    if PersistentNodeSet is None:
-        return
-
-    with PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as node_set:
-        node_set.add(node)
-        assert node_set.get(object_=node.object, predicate=node.predicate, subject=node.subject) == node
+    def test_get_extant(node: Node, tmpdir):
+        with PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as node_set:
+            node_set.add(node)
+            assert node_set.get(object_=node.object, predicate=node.predicate, subject=node.subject) == node
 
 
-def test_get_nonextant(node: Node, tmpdir):
-    if PersistentNodeSet is None:
-        return
-
-    with PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as node_set:
-        assert node_set.get(object_=node.object, predicate=node.predicate, subject=node.subject) is None
+    def test_get_nonextant(node: Node, tmpdir):
+        with PersistentNodeSet(name=tmpdir.mkdir("test"), create_if_missing=True) as node_set:
+            assert node_set.get(object_=node.object, predicate=node.predicate, subject=node.subject) is None
