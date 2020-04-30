2e746867e3761f38c50120951c88d4fc8fa42a52
Minor Gordon <gordom6@rpi.edu>
2020-04-30 13:31:17 -0400

    integrate new data structures into PipelineWrapper

--- mowgli/lib/etl/pipeline_wrapper.py
+++ mowgli/lib/etl/pipeline_wrapper.py
@@ -1,10 +1,19 @@
 import logging
+from pathlib import Path
+from tempfile import TemporaryDirectory
 from typing import Generator, Union, Dict, Optional
 
 from mowgli.lib.cskg.edge import Edge
 from mowgli.lib.cskg.node import Node
 from mowgli.lib.etl._pipeline import _Pipeline
 from mowgli.lib.etl.pipeline_storage import PipelineStorage
+
+try:
+    from mowgli.lib.storage.persistent_edge_set import PersistentEdgeSet
+    from mowgli.lib.storage.persistent_node_id_set import PersistentNodeIdSet
+    from mowgli.lib.storage.persistent_node_set import PersistentNodeSet
+except ImportError:
+    PersistentEdgeSet = PersistentNodeSet = PersistentNodeIdSet = None
 
 
 class PipelineWrapper:
@@ -50,52 +59,51 @@ class PipelineWrapper:
     ) -> Generator[Union[Edge, Node], None, None]:
         transform_generator = self.__pipeline.transformer.transform(**extract_kwds)
 
-        if skip_whole_graph_check:
-            self._logger.info("Skipping graph checking during transform")
+        if skip_whole_graph_check or PersistentEdgeSet is None:
+            self._logger.info("skipping whole graph checking during transform")
             yield from transform_generator
             return
 
-        edges_by_signature = {}
-        nodes_by_id = {}
-        node_ids_used_by_edges = set()
-
-        for node_or_edge in transform_generator:
-            if isinstance(node_or_edge, Node):
-                node = node_or_edge
-                # Node ID's should be unique in the CSKG.
-                existing_node = nodes_by_id.get(node.id)
-                if existing_node is not None:
-                    if existing_node == node:
-                        # Common case: ignore exact duplicate nodes i.e., nodes that are the same in all fields.
-                        # This happens frequently in the word association sources, where the same word can come
-                        # up as a response to multiple cues.
-                        continue
-                    else:
-                        # Throw an exception if two nodes have the same id but aren't the same in all of their fields
-                        raise ValueError(
-                            "nodes with same id, different contents: original=%s, duplicate=%s"
-                            % (existing_node, node)
-                        )
-                else:
-                    nodes_by_id[node.id] = node
-            elif isinstance(node_or_edge, Edge):
-                edge = node_or_edge
-                # Edges should be unique in the CSKG, meaning that the tuple of (subject, predicate, object) should be unique.
-                existing_subject_edges = edges_by_signature.setdefault(edge.subject, {})
-                existing_predicate_edges = existing_subject_edges.setdefault(
-                    edge.predicate, {}
-                )
-                existing_object_edge = existing_predicate_edges.get(edge.object)
-                if existing_object_edge is not None:
-                    # Don't try to handle the exact duplicate case differently. It should never happen.
-                    raise ValueError(
-                        "duplicate edge: original=%s, duplicate=%s"
-                        % (existing_object_edge, edge)
-                    )
-                existing_predicate_edges[edge.object] = edge
-                node_ids_used_by_edges.add(edge.subject)
-                node_ids_used_by_edges.add(edge.object)
-            yield node_or_edge
-        for node_id in nodes_by_id.keys():
-            if node_id not in node_ids_used_by_edges:
-                raise ValueError("node %s not used by an edges: " % node_id)
+        with TemporaryDirectory() as temp_dir:
+            temp_dir_path = Path(temp_dir)
+            with PersistentEdgeSet(name=temp_dir_path / "edges", create_if_missing=True) as edge_set:
+                with PersistentNodeSet(name=temp_dir_path / "nodes", create_if_missing=True) as node_set:
+                    with PersistentNodeIdSet(name=temp_dir_path / "used_node_ids",
+                                             create_if_missing=True) as used_node_ids_set:
+                        for node_or_edge in transform_generator:
+                            if isinstance(node_or_edge, Node):
+                                node = node_or_edge
+                                # Node ID's should be unique in the CSKG.
+                                existing_node = node_set.get(node.id)
+                                if existing_node is not None:
+                                    if existing_node == node:
+                                        # Common case: ignore exact duplicate nodes i.e., nodes that are the same in all fields.
+                                        # This happens frequently in the word association sources, where the same word can come
+                                        # up as a response to multiple cues.
+                                        continue
+                                    else:
+                                        # Throw an exception if two nodes have the same id but aren't the same in all of their fields
+                                        raise ValueError(
+                                            "nodes with same id, different contents: original=%s, duplicate=%s"
+                                            % (existing_node, node)
+                                        )
+                                else:
+                                    node_set.add(node)
+                            elif isinstance(node_or_edge, Edge):
+                                edge = node_or_edge
+                                # Edges should be unique in the CSKG, meaning that the tuple of (subject, predicate, object) should be unique.
+                                existing_edge = edge_set.get(object_=edge.object, predicate=edge.predicate,
+                                                             subject=edge.subject)
+                                if existing_edge is not None:
+                                    # Don't try to handle the exact duplicate case differently. It should never happen.
+                                    raise ValueError(
+                                        "duplicate edge: original=%s, duplicate=%s"
+                                        % (existing_edge, edge)
+                                    )
+                                edge_set.add(edge)
+                                used_node_ids_set.add(edge.subject)
+                                used_node_ids_set.add(edge.object)
+                            yield node_or_edge
+                        for node_id in node_set.keys():
+                            if node_id not in used_node_ids_set:
+                                raise ValueError("node %s not used by an edges: " % node_id)
--- mowgli/lib/storage/persistent_node_set.py
+++ mowgli/lib/storage/persistent_node_set.py
@@ -1,5 +1,5 @@
 import pickle
-from typing import Optional
+from typing import Optional, Generator
 
 from mowgli.lib.cskg.node import Node
 from mowgli.lib.storage._leveldb import _Leveldb
@@ -7,17 +7,22 @@ from mowgli.lib.storage._leveldb import _Leveldb
 
 class PersistentNodeSet(_Leveldb):
     def add(self, node: Node) -> None:
-        key = self.__key(node_id=node.id)
+        key = self.__key(node.id)
         value = pickle.dumps(node)
         self._db.put(key, value)
 
-    def get(self, *, node_id: str) -> Optional[Node]:
-        key = self.__key(node_id=node_id)
+    def get(self, node_id: str) -> Optional[Node]:
+        key = self.__key(node_id)
         value = self._db.get(key)
         if value is not None:
             return pickle.loads(value)
         else:
             return None
 
-    def __key(self, *, node_id: str) -> bytes:
+    def __key(self, node_id: str) -> bytes:
         return node_id.encode("utf-8")
+
+    def keys(self) -> Generator[str, None, None]:
+        with self._db.iterator() as it:
+            for key, _ in it:
+                yield key.decode("utf-8")
