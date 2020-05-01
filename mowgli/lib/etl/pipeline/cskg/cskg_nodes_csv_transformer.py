47bf3c16b1ce09fd69acc0ca8d91b719e8fad2c2
Minor Gordon <gordom6@rpi.edu>
2020-05-01 17:02:41 -0400

    refactor CskgCsvTransformer to simplify reuse

---
+++ mowgli/lib/etl/pipeline/cskg/cskg_nodes_csv_transformer.py
@@ -0,0 +1,29 @@
+import csv
+from pathlib import Path
+from typing import Union, TextIO, Generator
+
+from mowgli.lib.cskg.node import Node
+from mowgli.lib.etl.pipeline._csv_transformer import _CsvTransformer
+
+
+class CskgNodesCsvTransformer(_CsvTransformer):
+    def transform(self, *, nodes_csv_file: Union[Path, TextIO]) -> Generator[Node, None, None]:
+        if isinstance(nodes_csv_file, Path):
+            nodes_csv_file_path = nodes_csv_file
+            with open(nodes_csv_file_path) as nodes_csv_file:
+                yield from self.__transform(nodes_csv_file=nodes_csv_file)
+        else:
+            yield from self.__transform(nodes_csv_file=nodes_csv_file)
+
+    def __transform(self, *, nodes_csv_file: TextIO) -> Generator[Node, None, None]:
+        csv_reader = csv.DictReader(nodes_csv_file, delimiter="\t", quoting=csv.QUOTE_NONE)
+        for csv_row in csv_reader:
+            yield \
+                Node(
+                    aliases=self._get_optional_column(csv_row, "aliases"),
+                    datasource=self._get_required_column(csv_row, "datasource"),
+                    id=self._get_required_column(csv_row, "id"),
+                    label=self._get_optional_column(csv_row, "label"),
+                    other=self._get_optional_column(csv_row, "other"),
+                    pos=self._get_optional_column(csv_row, "pos"),
+                )
