2477dff645c8dbbd81e26af4c9a928ebc2f8fb23
Minor Gordon <gordom6@rpi.edu>
2020-05-04 11:20:22 -0400

    make tests conditional

--- tests/mowgli_test/lib/etl/mapper/concept_net/test_concept_net_index.py
+++ tests/mowgli_test/lib/etl/mapper/concept_net/test_concept_net_index.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 
-import pytest
+from mowgli import paths
 
-from mowgli.lib.etl.mapper.concept_net.concept_net_index import ConceptNetIndex
+try:
+    from mowgli.lib.etl.mapper.concept_net.concept_net_index import ConceptNetIndex
+except ImportError:
+    ConceptNetIndex = None
+
+if ConceptNetIndex is not None and (paths.DATA_DIR / "cskg_release" / "extracted").is_dir():
+    def concept_net_index(tmpdir):
+        return ConceptNetIndex(Path(tmpdir), limit=10000, report_progress=True)
 
 
-@pytest.fixture
-def concept_net_index(tmpdir):
-    return ConceptNetIndex(Path(tmpdir), limit=10000, report_progress=True)
-
-
-def test_get(concept_net_index):
-    # There is only one "a" label
-    assert concept_net_index.get("a") == "/c/en/a"
-    # There are multiple "30" labels, return the unqualified node id
-    assert concept_net_index.get("30") == "/c/en/30"
-    # Return the "30" adjective
-    assert concept_net_index.get("30", pos="a") == "/c/en/30/a/wn"
-    # There's no "30" noun, should return the unqualified node id as above
-    assert concept_net_index.get("30", pos="v") == "/c/en/30"
+    def test_get(tmpdir):
+        with concept_net_index(tmpdir) as index:
+            # There is only one "a" label
+            assert index.get("a") == "/c/en/a"
+            # There are multiple "30" labels, return the unqualified node id
+            assert index.get("30") == "/c/en/30"
+            # Return the "30" adjective
+            assert index.get("30", pos="a") == "/c/en/30/a/wn"
+            # There's no "30" noun, should return the unqualified node id as above
+            assert index.get("30", pos="v") == "/c/en/30"
