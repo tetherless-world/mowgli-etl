c769535e8cc6cf9abf322244bdba9278882de54f
Minor Gordon <gordom6@rpi.edu>
2020-05-04 12:31:05 -0400

    make LevelDb public so it can be used in composition

--- mowgli/lib/storage/level_db.py
+++ mowgli/lib/storage/level_db.py
@@ -8,11 +8,15 @@ import plyvel
 from mowgli.lib.storage._closeable import _Closeable
 
 
-class _Leveldb(_Closeable):
+class LevelDb(_Closeable):
     def __init__(self, *, name: Union[str, Path], create_if_missing=True, delete_on_close=False, **kwds):
         self._db = plyvel.DB(name=str(name), create_if_missing=create_if_missing, **kwds)
         self.__delete_on_close = delete_on_close
         self.__name = name
+
+    @property
+    def db(self) -> plyvel.DB:
+        return self._db
 
     def close(self):
         self._db.close()
--- mowgli/lib/storage/persistent_node_id_set.py
+++ mowgli/lib/storage/persistent_node_id_set.py
@@ -1,11 +1,11 @@
-from mowgli.lib.storage._leveldb import _Leveldb
 from mowgli.lib.storage._node_id_set import _NodeIdSet
+from mowgli.lib.storage.level_db import LevelDb
 
 
-class PersistentNodeIdSet(_NodeIdSet, _Leveldb):
+class PersistentNodeIdSet(_NodeIdSet, LevelDb):
     def __init__(self, **kwds):
         _NodeIdSet.__init__(self)
-        _Leveldb.__init__(self, **kwds)
+        LevelDb.__init__(self, **kwds)
 
     def add(self, node_id: str) -> None:
         key = self.__construct_node_key(node_id=node_id)
