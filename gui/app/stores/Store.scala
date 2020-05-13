dc879f6cc0d282537576d9eb157bbe47604e9bf6
Minor Gordon <gordom6@rpi.edu>
2020-05-13 14:46:30 -0400

    fix Store

--- gui/app/stores/Store.scala
+++ gui/app/stores/Store.scala
@@ -1,7 +1,6 @@
 package stores
 
 import com.google.inject.ImplementedBy
-import io.github.tetherlessworld.dsa_models.policy.{Policy, PolicyId}
 
 @ImplementedBy(classOf[PlaceholderStore])
 trait Store {
