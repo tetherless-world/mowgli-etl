4a18e08bfafaeb0c7cab9f4257d42d6996a4ee3c
Minor Gordon <gordom6@rpi.edu>
2020-05-18 15:10:05 -0400

    change Node and Edge case classes to not have default values, in order to support compiler checking elsewhere

--- gui/app/models/cskg/Edge.scala
+++ gui/app/models/cskg/Edge.scala
@@ -3,8 +3,8 @@ package models.cskg
 final case class Edge(
                       datasource: String,
                       `object`: String,
+                      other: Option[String], // No default values so the compiler can check missing fields on construction
                       predicate: String,
                       subject: String,
-                      other: Option[String] = None,
-                      weight: Option[Float] = None
+                      weight: Option[Float]
                      )
