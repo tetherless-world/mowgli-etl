c385c8e8e6f6f125b3b844fb5bbdf271a8717100
123joshuawu <joshuawu3@gmail.com>
2020-05-21 18:46:03 -0700

    Add apollo queries for matching nodes

---
+++ gui/gui/src/ts/api/queries/types/MatchingNodesCountQuery.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL query operation: MatchingNodesCountQuery
+// ====================================================
+
+export interface MatchingNodesCountQuery {
+  matchingNodesCount: number;
+}
+
+export interface MatchingNodesCountQueryVariables {
+  text: string;
+}
---
+++ gui/gui/src/ts/api/queries/types/NodeFieldsFragment.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+// This file was automatically generated and should not be edited.
+
+// ====================================================
+// GraphQL fragment: NodeFieldsFragment
+// ====================================================
+
+export interface NodeFieldsFragment {
+  __typename: "Node";
+  aliases: string[] | null;
+  datasource: string;
+  id: string;
+  label: string;
+  other: string | null;
+  pos: string | null;
+}
