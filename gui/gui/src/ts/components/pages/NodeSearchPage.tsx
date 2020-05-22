d7e3c4f9448fda6d79f27f767238f7d3cc9eda8f
123joshuawu <joshuawu3@gmail.com>
2020-05-22 14:35:57 -0700

    Refactor to useApolloClient

--- gui/gui/src/ts/components/pages/NodeSearchPage.tsx
+++ gui/gui/src/ts/components/pages/NodeSearchPage.tsx
@@ -4,50 +4,94 @@ import {Frame} from "components/frame/Frame";
 import {SearchTextInput} from "components/search/SearchTextInput";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faFilter} from "@fortawesome/free-solid-svg-icons";
-import {useQuery} from "@apollo/react-hooks";
-import {MatchingNodesQuery} from "api/queries/types/MatchingNodesQuery";
-import * as matchingNodesQuery from "api/queries/MatchingNodesQuery.graphql";
+import {useApolloClient} from "@apollo/react-hooks";
+import {
+  MatchingNodesQuery,
+  MatchingNodesQueryVariables,
+} from "api/queries/types/MatchingNodesQuery";
+import * as MatchingNodesQueryDocument from "api/queries/MatchingNodesQuery.graphql";
 import {NodeTable} from "components/data/NodeTable";
+import {Node} from "models/Node";
+
+interface MatchingNodesQueryState {
+  matchingNodes: Node[];
+  searchText: string;
+  count: number;
+  offset: number;
+  queryLimit: number;
+}
 
 export const NodeSearchPage: React.FunctionComponent<{}> = ({}) => {
-  const [searchText, setSearchText] = React.useState<string>("");
-  const [matchingNodesCount, setMatchingNodesCount] = React.useState<
-    number | null
-  >(null);
-  const [matchingNodesPage, setMatchingNodesPage] = React.useState<number>(0);
-  const [matchingNodesQueryLimit, setMatchingNodesQueryLimit] = React.useState<
-    number
-  >(10);
-
-  const onSearchSubmit = (searchText: string) => {
-    setSearchText(searchText);
-    setMatchingNodesCount(null);
-  };
-
-  const {data, loading} = useQuery<MatchingNodesQuery>(matchingNodesQuery, {
-    variables: {
-      limit: matchingNodesQueryLimit,
-      offset: matchingNodesPage,
-      text: searchText,
-      withCount: matchingNodesCount === null,
-    },
-    skip: searchText.length === 0,
+  const [state, setState] = React.useState<MatchingNodesQueryState>({
+    matchingNodes: [],
+    searchText: "",
+    count: 0,
+    offset: 0,
+    queryLimit: 10,
   });
 
-  const matchingNodes = data?.matchingNodes;
+  const onLoadedData = (updatedState: Partial<MatchingNodesQueryState>) =>
+    setState((prevState) => ({...prevState, ...updatedState}));
 
-  if (data && data.matchingNodesCount && matchingNodesCount === null) {
-    setMatchingNodesCount(data!.matchingNodesCount!);
-  }
+  const apolloClient = useApolloClient();
+
+  const onSearchSubmit = (searchText: string) =>
+    apolloClient
+      .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
+        query: MatchingNodesQueryDocument,
+        variables: {
+          limit: state.queryLimit,
+          offset: state.offset,
+          text: searchText,
+          withCount: true,
+        },
+      })
+      .then(({data}) =>
+        onLoadedData({
+          matchingNodes: data.matchingNodes,
+          count: data.matchingNodesCount,
+          searchText: searchText,
+        })
+      );
 
   const onTableChangePage = (newPage: number) => {
-    setMatchingNodesPage(newPage);
+    const newOffset = newPage * state.queryLimit;
+
+    apolloClient
+      .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
+        query: MatchingNodesQueryDocument,
+        variables: {
+          limit: state.queryLimit,
+          offset: newOffset,
+          text: state.searchText,
+          withCount: false,
+        },
+      })
+      .then(({data}) =>
+        onLoadedData({matchingNodes: data.matchingNodes, offset: newOffset})
+      );
   };
 
-  const onTableChangeRowsPerPage = (newRowsPerPage: number) => {
-    setMatchingNodesQueryLimit(newRowsPerPage);
-    setMatchingNodesPage(0);
-  };
+  const onTableChangeRowsPerPage = (newRowsPerPage: number) =>
+    apolloClient
+      .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
+        query: MatchingNodesQueryDocument,
+        variables: {
+          limit: newRowsPerPage,
+          offset: 0,
+          text: state.searchText,
+          withCount: false,
+        },
+      })
+      .then(({data}) =>
+        onLoadedData({
+          matchingNodes: data.matchingNodes,
+          offset: 0,
+          queryLimit: newRowsPerPage,
+        })
+      );
+
+  const {matchingNodes, count, searchText, queryLimit, offset} = state;
 
   return (
     <Frame>
@@ -62,17 +106,16 @@ export const NodeSearchPage: React.FunctionComponent<{}> = ({}) => {
           </Button>
         </Grid>
         <Grid item xs={8} data-cy="visualizationContainer">
-          {loading && "Loading"}
-          {matchingNodes && matchingNodesCount && (
+          {searchText.length > 0 && (
             <React.Fragment>
               <Typography variant="body1">
-                {matchingNodesCount} results for "{searchText}"
+                {count} results for "{searchText}"
               </Typography>
               <NodeTable
                 nodes={matchingNodes}
-                rowsPerPage={matchingNodesQueryLimit}
-                count={matchingNodesCount}
-                page={matchingNodesPage}
+                rowsPerPage={queryLimit}
+                count={count}
+                page={offset / queryLimit}
                 onChangePage={onTableChangePage}
                 onChangeRowsPerPage={onTableChangeRowsPerPage}
               />
