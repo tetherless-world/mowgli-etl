89a001186d999934095307bf965385f8a584a915
123joshuawu <joshuawu3@gmail.com>
2020-05-27 15:16:11 -0700

    Move search to navbar

--- gui/gui/src/ts/components/navbar/Navbar.tsx
+++ gui/gui/src/ts/components/navbar/Navbar.tsx
@@ -1,12 +1,42 @@
 import * as React from "react";
 
-import {Typography, AppBar, Toolbar} from "@material-ui/core";
+import * as qs from "qs";
+
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  makeStyles,
+  createStyles,
+} from "@material-ui/core";
+
+import {SearchTextInput} from "components/search/SearchTextInput";
+import {useHistory} from "react-router-dom";
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    brand: {
+      marginRight: theme.spacing(2),
+    },
+  })
+);
 
 export const Navbar: React.FunctionComponent<{}> = () => {
+  const classes = useStyles();
+
+  const history = useHistory();
+
+  const onSearchSubmit = (search: string) => {
+    history.push(qs.stringify({search}, {addQueryPrefix: true}));
+  };
+
   return (
     <AppBar position="static">
       <Toolbar>
-        <Typography variant="h6">MOWGLI</Typography>
+        <Typography variant="h6" className={classes.brand}>
+          MOWGLI
+        </Typography>
+        <SearchTextInput onSubmit={onSearchSubmit} />
       </Toolbar>
     </AppBar>
   );
--- gui/gui/src/ts/components/pages/NodeSearchPage.tsx
+++ gui/gui/src/ts/components/pages/NodeSearchPage.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import {Grid, Typography} from "@material-ui/core";
 import {Frame} from "components/frame/Frame";
-import {SearchTextInput} from "components/search/SearchTextInput";
+// import {SearchTextInput} from "components/search/SearchTextInput";
 import {useApolloClient} from "@apollo/react-hooks";
 import {
   MatchingNodesQuery,
@@ -11,92 +11,107 @@ import * as MatchingNodesQueryDocument from "api/queries/MatchingNodesQuery.grap
 import {NodeTable} from "components/data/NodeTable";
 import {Node} from "models/Node";
 import * as ReactLoader from "react-loader";
+import {useLocation} from 'react-router-dom';
+import * as qs from 'qs';
 
-interface MatchingNodesQueryState {
-  matchingNodes: Node[];
-  searchText: string;
-  count: number;
-  offset: number;
-  queryLimit: number;
+interface Matching
+
+class MatchingNodesQueryState {
+  // matchingNodes: Node[];
+  private constructor (
+  public readonly searchText: string,
+  // public readonly count: number,
+  public readonly offset: number = 0,
+  public readonly queryLimit: number = 10,
+  ) {}
+
+  parse(queryString: string) {
+    const {search, offset, limit} = qs.parse(queryString, {ignoreQueryPrefix:true})
+    return new MatchingNodesQueryState(search as string, offset, limit);
+  }
 }
 
 export const NodeSearchPage: React.FunctionComponent<{}> = ({}) => {
-  const [state, setState] = React.useState<MatchingNodesQueryState>({
-    matchingNodes: [],
-    searchText: "",
-    count: 0,
-    offset: 0,
-    queryLimit: 10,
-  });
+  const location = useLocation();
 
-  const [loading, setLoading] = React.useState<boolean>(false);
+  const locationQueryObject = ((qs.parse(location.search, {ignoreQueryPrefix:true}) as unknown) as MatchingNodesQuery);
 
-  const onLoadedData = (updatedState: Partial<MatchingNodesQueryState>) => {
-    setState((prevState) => ({...prevState, ...updatedState}));
-    setLoading(false);
-  };
+  // const [state, setState] = React.useState<MatchingNodesQueryState>({
+  //   matchingNodes: [],
+  //   // searchText: "",
+  //   count: 0,
+  //   offset: 0,
+  //   queryLimit: 10,
+  // });
 
-  const apolloClient = useApolloClient();
+  // const [loading, setLoading] = React.useState<boolean>(false);
 
-  const onSearchSubmit = (searchText: string) => {
-    setLoading(true);
-    apolloClient
-      .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
-        query: MatchingNodesQueryDocument,
-        variables: {
-          limit: state.queryLimit,
-          offset: state.offset,
-          text: searchText,
-          withCount: true,
-        },
-      })
-      .then(({data}) =>
-        onLoadedData({
-          matchingNodes: data.matchingNodes,
-          count: data.matchingNodesCount,
-          searchText: searchText,
-        })
-      );
-  };
+  // const onLoadedData = (updatedState: Partial<MatchingNodesQuery>) => {
+  //   setState((prevState) => ({...prevState, ...updatedState}));
+  //   setLoading(false);
+  // };
+
+  // const apolloClient = useApolloClient();
+
+  // const onSearchSubmit = (searchText: string) => {
+  //   setLoading(true);
+  //   apolloClient
+  //     .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
+  //       query: MatchingNodesQueryDocument,
+  //       variables: {
+  //         limit: state.queryLimit,
+  //         offset: state.offset,
+  //         text: searchText,
+  //         withCount: true,
+  //       },
+  //     })
+  //     .then(({data}) =>
+  //       onLoadedData({
+  //         matchingNodes: data.matchingNodes,
+  //         count: data.matchingNodesCount,
+  //         searchText: searchText,
+  //       })
+  //     );
+  // };
 
   const onTableChangePage = (newPage: number) => {
     const newOffset = newPage * state.queryLimit;
 
-    apolloClient
-      .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
-        query: MatchingNodesQueryDocument,
-        variables: {
-          limit: state.queryLimit,
-          offset: newOffset,
-          text: state.searchText,
-          withCount: false,
-        },
-      })
-      .then(({data}) =>
-        onLoadedData({matchingNodes: data.matchingNodes, offset: newOffset})
-      );
+    // apolloClient
+    //   .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
+    //     query: MatchingNodesQueryDocument,
+    //     variables: {
+    //       limit: state.queryLimit,
+    //       offset: newOffset,
+    //       text: searchText,
+    //       withCount: false,
+    //     },
+    //   })
+    //   .then(({data}) =>
+    //     onLoadedData({matchingNodes: data.matchingNodes, offset: newOffset})
+    //   );
   };
 
-  const onTableChangeRowsPerPage = (newRowsPerPage: number) =>
-    apolloClient
-      .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
-        query: MatchingNodesQueryDocument,
-        variables: {
-          limit: newRowsPerPage,
-          offset: 0,
-          text: state.searchText,
-          withCount: false,
-        },
-      })
-      .then(({data}) =>
-        onLoadedData({
-          matchingNodes: data.matchingNodes,
-          offset: 0,
-          queryLimit: newRowsPerPage,
-        })
-      );
+  const onTableChangeRowsPerPage = (newRowsPerPage: number) => {}
+    // apolloClient
+    //   .query<MatchingNodesQuery, MatchingNodesQueryVariables>({
+    //     query: MatchingNodesQueryDocument,
+    //     variables: {
+    //       limit: newRowsPerPage,
+    //       offset: 0,
+    //       text: searchText,
+    //       withCount: false,
+    //     },
+    //   })
+    //   .then(({data}) =>
+    //     onLoadedData({
+    //       matchingNodes: data.matchingNodes,
+    //       offset: 0,
+    //       queryLimit: newRowsPerPage,
+    //     })
+    //   );
 
-  const {matchingNodes, count, searchText, queryLimit, offset} = state;
+  const {matchingNodes, count, queryLimit, offset} = state;
 
   return (
     <Frame>
@@ -105,7 +120,7 @@ export const NodeSearchPage: React.FunctionComponent<{}> = ({}) => {
           <Typography variant="body1">
             Searching <span>all data sources</span>
           </Typography>
-          <SearchTextInput onSubmit={onSearchSubmit} />
+          {/* <SearchTextInput onSubmit={onSearchSubmit} /> */}
           {/* <Button>
             <FontAwesomeIcon icon={faFilter} /> &nbsp;Filters
           </Button> */}
