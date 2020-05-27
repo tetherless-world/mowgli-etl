d6deb5bb3de3683dd9427514cdaa8b35a224eff9
Minor Gordon <gordom6@rpi.edu>
2020-05-27 16:33:56 -0400

    separate Routse.tsx

--- gui/gui/src/ts/main.tsx
+++ gui/gui/src/ts/main.tsx
@@ -1,12 +1,12 @@
 import {apolloClient} from "api/apolloClient";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import {ApolloProvider} from "react-apollo";
 import {ApolloProvider as ApolloHooksProvider} from "@apollo/react-hooks";
 import {CssBaseline} from "@material-ui/core";
 import {ConsoleLogger, LoggerContext} from "@tetherless-world/twxplore-base";
-import {NodeSearchPage} from "./components/pages/NodeSearchPage";
+import {Routes} from "./Routes";
 
 // Logger
 const logger = new ConsoleLogger();
@@ -17,8 +17,7 @@ ReactDOM.render(
       <LoggerContext.Provider value={logger}>
         <BrowserRouter>
           <CssBaseline />
-          {/*<Route component={Frame}></Route>*/}
-          <Route component={NodeSearchPage}></Route>
+          <Routes/>
         </BrowserRouter>
       </LoggerContext.Provider>
     </ApolloHooksProvider>
