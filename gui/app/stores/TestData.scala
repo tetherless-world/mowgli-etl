376c029f5fce55be8dedecfa55a07aed2665a4c3
Minor Gordon <gordom6@rpi.edu>
2020-06-01 12:08:20 -0400

    deduplicate nodes and edges and remove dangling edges

--- gui/app/stores/TestData.scala
+++ gui/app/stores/TestData.scala
@@ -7,15 +7,16 @@ import models.cskg.{Edge, Node}
 import scala.collection.mutable
 
 object TestData {
-  val edges = deduplicateEdges(readEdges())
-  val nodes = deduplicateNodes(readNodes())
+  private val nodesById = deduplicateNodes(readNodes())
+  val nodes = nodesById.values.toList
+  val edges = removeDanglingEdges(deduplicateEdges(readEdges()), nodesById)
 
   private def deduplicateEdges(edges: List[Edge]): List[Edge] =
     // Default toMap duplicate handling = use later key
     edges.map(edge => ((edge.subject, edge.predicate, edge.`object`), edge)).toMap.values.toList
 
-  private def deduplicateNodes(nodes: List[Node]): List[Node] =
-    nodes.map(node => (node.id, node)).toMap.values.toList
+  private def deduplicateNodes(nodes: List[Node]): Map[String, Node] =
+    nodes.map(node => (node.id, node)).toMap
 
   private def readEdges(): List[Edge] = {
     val edgesCsvInputStream = getClass.getResourceAsStream("/test_data/edges.csv")
@@ -34,4 +35,7 @@ object TestData {
       nodesCsvInputStream.close()
     }
   }
+
+  private def removeDanglingEdges(edges: List[Edge], nodesById: Map[String, Node]): List[Edge] =
+    edges.filter(edge => nodesById.contains(edge.subject) && nodesById.contains(edge.`object`))
 }
