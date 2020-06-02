3117c17d3c7e21f04334107da05c44554dd88e81
Minor Gordon <gordom6@rpi.edu>
2020-06-02 15:45:59 -0400

    fix integration test

--- gui/test/integration/cypress/integration/RandomNodePage.spec.ts
+++ gui/test/integration/cypress/integration/RandomNodePage.spec.ts
@@ -3,9 +3,8 @@ import {RandomNodePage} from "../support/page_files/RandomNodePage";
 context("Random node page", () => {
   const page = new RandomNodePage();
 
-  beforeEach(() => page.visit());
-
   it("should immediately redirect to a node page", () => {
+    cy.visit(page.relativeUrl);
     cy.url().should("contains", Cypress.config().baseUrl + "/node/");
   });
 });
