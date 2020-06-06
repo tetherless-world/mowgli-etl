0e26698a30fbc571e5b294ecf1a8896306b0cf19
123joshuawu <joshuawu3@gmail.com>
2020-06-05 19:31:19 -0700

    Add TestData for info on test data

--- gui/test/integration/cypress/integration/HomePage.spec.ts
+++ gui/test/integration/cypress/integration/HomePage.spec.ts
@@ -1,13 +1,15 @@
 import {HomePage} from "../support/page_files/HomePage";
+import {TestData} from "../support/TestData";
 
 context("Home Page", () => {
+  const data = new TestData();
   const page = new HomePage();
 
   beforeEach(() => page.visit());
 
   it("should show total node and edge counts", () => {
-    page.totalNodeCount.should("have.text", "1000 nodes");
-    page.totalEdgeCount.should("have.text", "52293 relationships");
+    page.totalNodeCount.should("have.text", `${data.nodeCount} nodes`);
+    page.totalEdgeCount.should("have.text", `${data.edgeCount} relationships`);
   });
 
   it("should show all datasources", () => {
@@ -15,8 +17,8 @@ context("Home Page", () => {
   });
 
   it("should show selected datasource", () => {
-    page.search.selectDatasource("gui_test_data");
+    page.search.selectDatasource(data.datasources[0]);
 
-    page.search.selectedDatasource.should("have.text", "gui_test_data");
+    page.search.selectedDatasource.should("have.text", data.datasources[0]);
   });
 });
