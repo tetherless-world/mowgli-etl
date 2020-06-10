2cb3acf8e6489fccced22958a1c17fa130f30d2f
Minor Gordon <gordom6@rpi.edu>
2020-06-09 20:24:05 -0400

    move loader tests to mirror directory

--- tests/mowgli_etl_test/loader/rdf/triple_rdf_loader_test.py
+++ tests/mowgli_etl_test/loader/rdf/triple_rdf_loader_test.py
@@ -5,7 +5,7 @@ from mowgli_etl.model.edge import Edge
 from mowgli_etl.loader.rdf.triple_rdf_loader import TripleRdfLoader
 from mowgli_etl.pipeline_storage import PipelineStorage
 from mowgli_etl.pipeline_wrapper import PipelineWrapper
-from tests.mowgli_etl_test.pipeline.rdf.rdf_loader_test_pipeline import RdfLoaderTestPipeline
+from tests.mowgli_etl_test.loader.rdf.rdf_loader_test_pipeline import RdfLoaderTestPipeline
 
 
 @pytest.fixture
