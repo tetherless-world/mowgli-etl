8793ac1fb39986e7c29cf3970008d89f189caa33
Minor Gordon <gordom6@rpi.edu>
2020-06-09 21:33:30 -0400

    add Path to Model

--- mowgli_etl/model/model.py
+++ mowgli_etl/model/model.py
@@ -2,5 +2,6 @@ from typing import Union
 
 from mowgli_etl.model.edge import Edge
 from mowgli_etl.model.node import Node
+from mowgli_etl.model.path import Path
 
-Model = Union[Edge, Node]
+Model = Union[Edge, Node, Path]
