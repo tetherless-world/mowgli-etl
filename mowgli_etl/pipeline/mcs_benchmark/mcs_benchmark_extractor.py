903df4cf1aa32bbed476f825a192a860a77b6dd0
Sam Stouffer <samuellstouffer@gmail.com>
2020-06-23 16:27:31 -0400

    Configure mcs benchmark extractor to use local benchmark repo files

--- mowgli_etl/pipeline/mcs_benchmark/mcs_benchmark_extractor.py
+++ mowgli_etl/pipeline/mcs_benchmark/mcs_benchmark_extractor.py
@@ -1,7 +1,7 @@
 from typing import Dict, Optional
 
 from mowgli_etl._extractor import _Extractor
-from mowgli_etl.paths import DATA_DIR
+from mowgli_etl.paths import DATA_DIR, PROJECT_ROOT
 from mowgli_etl.pipeline_storage import PipelineStorage
 
 
@@ -9,9 +9,12 @@ class McsBenchmarkExtractor(_Extractor):
     def extract(
         self, *, force: bool, storage: PipelineStorage
     ) -> Optional[Dict[str, object]]:
+        jsonl_paths = []
+        benchmark_project_path =  PROJECT_ROOT.parent / "CommonsenseBenchmark"
+        if benchmark_project_path.exists():
+            for benchmark_path in (benchmark_project_path / "converted").iterdir():
+                for benchmark_jsonl_path in benchmark_path.glob("*.jsonl"):
+                    jsonl_paths.append(benchmark_jsonl_path)
         return {
-            "benchmark_sample_path": DATA_DIR
-            / "mcs_benchmark"
-            / "extracted"
-            / "PhysicalIQA_dev.jsonl"
+            "benchmark_jsonl_paths": tuple(jsonl_paths)
         }
--- mowgli_etl/pipeline/mcs_benchmark/mcs_benchmark_transformer.py
+++ mowgli_etl/pipeline/mcs_benchmark/mcs_benchmark_transformer.py
@@ -1,8 +1,9 @@
 import json
 from pathlib import Path
-from typing import Generator
+from typing import Dict, Generator, Tuple
 
 from mowgli_etl._transformer import _Transformer
+from mowgli_etl.model.benchmark_answer import BenchmarkAnswer
 from mowgli_etl.model.benchmark_question import BenchmarkQuestion
 from mowgli_etl.model.benchmark_question_choice import BenchmarkQuestionChoice
 from mowgli_etl.model.model import Model
@@ -14,30 +15,52 @@ class McsBenchmarkTransformer(_Transformer):
     _QUESTION_TYPES = {"BenchmarkGoal", "BenchmarkQuestion", "BenchmarkObservation"}
     _CORRECT_CHOICE_LABEL = "correctChoiceLabel"
 
-    def transform(self, benchmark_sample_path: Path) -> Generator[Model, None, None]:
-        with open(benchmark_sample_path) as benchmark_sample_file:
-            for json_txt in benchmark_sample_file.readlines():
-                benchmark_sample = json.loads(json_txt)
-                question_text = ""
-                for antecedent_item in benchmark_sample["antecedent"][self._LIST]:
-                    antecedent_type = antecedent_item[self._TYPE]
-                    if antecedent_type in self._QUESTION_TYPES:
-                        question_text += antecedent_item["text"]
-                correct_choice_label = None
-                if self._CORRECT_CHOICE_LABEL in benchmark_sample:
-                    correct_choice_label = benchmark_sample[self._CORRECT_CHOICE_LABEL]
+    def __init__(self):
+        super().__init__()
+        self.__transformers = {
+            "BenchmarkSample": self.transform_benchmark_sample,
+            "SubmissionSample": self.transform_submission_sample,
+        }
 
-                choices = tuple(
-                    BenchmarkQuestionChoice(label=choice["name"], text=choice["text"])
-                    for choice in benchmark_sample["choices"][self._LIST]
-                )
+    def transform(
+        self, benchmark_jsonl_paths: Tuple[Path]
+    ) -> Generator[Model, None, None]:
+        for jsonl_path in benchmark_jsonl_paths:
+            with open(jsonl_path) as jsonl_file:
+                for line in jsonl_file.readlines():
+                    resource = json.loads(line)
+                    resource_type = resource[self._TYPE]
+                    if resource_type not in self.__transformers:
+                        raise ValueError(f"Unhandled top level type: {resource_type}")
+                    transformer = self.__transformers[resource_type]
+                    yield from transformer(resource)
 
-                yield BenchmarkQuestion(
-                    id=benchmark_sample["@id"],
-                    dataset_id=benchmark_sample["includedInDataset"],
-                    text=question_text,
-                    correct_choice_label=correct_choice_label,
-                    choices=choices
-                )
+    def transform_benchmark_sample(
+        self, benchmark_sample_json
+    ) -> Generator[BenchmarkQuestion, None, None]:
+        question_text = ""
+        for antecedent_item in benchmark_sample_json["antecedent"][self._LIST]:
+            antecedent_type = antecedent_item[self._TYPE]
+            if antecedent_type in self._QUESTION_TYPES:
+                question_text += antecedent_item["text"]
+        correct_choice_label = None
+        if self._CORRECT_CHOICE_LABEL in benchmark_sample_json:
+            correct_choice_label = benchmark_sample_json[self._CORRECT_CHOICE_LABEL]
 
+        choices = tuple(
+            BenchmarkQuestionChoice(label=choice["name"], text=choice["text"])
+            for choice in benchmark_sample_json["choices"][self._LIST]
+        )
 
+        yield BenchmarkQuestion(
+            id=benchmark_sample_json["@id"],
+            dataset_id=benchmark_sample_json["includedInDataset"],
+            text=question_text,
+            correct_choice_label=correct_choice_label,
+            choices=choices,
+        )
+
+    def transform_submission_sample(
+        self, submission_sample_json
+    ) -> Generator[BenchmarkAnswer, None, None]:
+        yield from []
