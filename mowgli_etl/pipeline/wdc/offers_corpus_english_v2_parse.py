f19795a375e90cb681ca8d1d8b6885afd771866c
Jesse E <ellinj2@rpi.edu>
2020-07-28 20:48:11 -0700

    Implemented rudimentary cleaning algorithm and compared name heuristics

--- mowgli_etl/pipeline/wdc/offers_corpus_english_v2_parse.py
+++ mowgli_etl/pipeline/wdc/offers_corpus_english_v2_parse.py
@@ -1,14 +1,32 @@
 import json
 import spacy
 import re
+from os import listdir
 
+print("\nLoading NLP library...")
 nlp = spacy.load("en_core_web_sm")
+
+# Generate a list of all possible jsonl files
+directory = [item for item in listdir() if "clean.jsonl" in item]
+if not directory:
+	print("Sorry, there don't seem to be any clean files. Please make sure to run offers_corpus_english_v2_clean.py, or have a file ending in \"clean.jsonl\"")
+	exit(1)
+
+# Present options for jsonl files
+print("Please choose which of the following files you'd like to clean [enter number on the left]:\n")
+for i in range(0, len(directory)):
+	print("\t{} - {}".format(i, directory[i]))
+
+# Have user select which file to clean
+choice = directory[int(input("\nChosen file number: "))]
 
 data = []
 
+i = 0
 # Read the source data file into a list of JSON data objects
-with open("offers_corpus_english_v2_random_100.jsonl") as file:
+with open(choice) as file:
 	for line in file:
+		i += 1
 		data.append(json.loads(line))
 
 count = 1
@@ -21,6 +39,13 @@ for line in data:
 	text = line["title"]
 	description = line["description"]
 	additional_info = line["specTableContent"]
+	category = line["category"]
+
+	# print("Options:")
+	# print(text)
+	# print(description)
+	# print(additional_info)
+	# print(category)
 
 	# Ignore product if there is no title
 	if text == None:
@@ -29,12 +54,33 @@ for line in data:
 	# Tokenize title
 	doc = nlp(text)
 
-	product_name = ""
+	last_noun_name = ""
+
+	first_noun_sequence_name = ""
+
+	last_noun_sequence_name = ""
 
 	# Assume that general product name is last noun in title
 	for token in doc:
 		if token.pos_ == "NOUN":
-			product_name = token.text
+			last_noun_name = token.text
+
+	# Assume that general product name is the first sequence of just nouns (token.pos between 92 and 100 inlcusive)
+	for token in doc:
+		if token.pos in range(92, 101):
+			if first_noun_sequence_name != "":
+				first_noun_sequence_name += " "
+			first_noun_sequence_name += token.text
+		elif first_noun_sequence_name != "":
+				break
+
+	first_noun_sequence_name.rstrip(" ")
+
+	"""
+	*
+	* TO DO: Implement last_noun_sequence heuristic
+	*
+	"""
 
 	dimensions = []
 
@@ -59,10 +105,19 @@ for line in data:
 				dimensions = re.findall("\d+\s?\w+\s\d+\s?\w+\slead\sx\s\d+\s?\w+", additional_info)
 
 	# Print out the results
-	if (product_name == ""):
+	print(text)
+	print("Using the last-noun heuristic: ", end='')
+	if (last_noun_name == ""):
 		print("No product name found.")
 	else:
-		print("The product is", product_name, end=".\n")
+		print("The product is \"{}\"".format(last_noun_name), end=".\n")
+
+	print("Using the first-noun-sequence heuristic: ", end='')
+	if (first_noun_sequence_name == ""):
+		print("No product name found.")
+	else:
+		print("The product is \"{}\"".format(first_noun_sequence_name), end=".\n")
+
 
 	if len(dimensions) == 0:
 		print("No dimensions found.", end="\n\n")
--- mowgli_etl/pipeline/wdc/offers_corpus_english_v2_random_sample.py
+++ mowgli_etl/pipeline/wdc/offers_corpus_english_v2_random_sample.py
@@ -1,6 +1,8 @@
 import random
+import os
 
-with open("offers_corpus_english_v2_1000.jsonl") as file:
+# new_path = os.path.relpath("../../../data//wdc/extract/offers_corpus_english_v2_1000.jsonl")
+with open(os.path.dirname(__file__) + "../../../data/wdc/extracted/offers_corpus_english_v2_1000.jsonl") as file:
 	lines = random.sample(file.readlines(), 100)
 
 with open("offers_corpus_english_v2_random_100.jsonl", "w") as file:
