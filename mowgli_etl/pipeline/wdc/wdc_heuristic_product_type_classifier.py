d71ca1f0778f23cc0f0c85a282eced17dbf7f17c
Jesse E <ellinj2@rpi.edu>
2020-09-16 19:59:24 -0700

    Updated product type and heuristic classifier

--- mowgli_etl/pipeline/wdc/wdc_heuristic_product_type_classifier.py
+++ mowgli_etl/pipeline/wdc/wdc_heuristic_product_type_classifier.py
@@ -7,7 +7,43 @@ class WdcHeuristicProductTypeClassifier(WdcProductTypeClassifier):
         '''
         Parse title/listing/other to pull ProductType with confidence value
         '''
+        doc = nlp(title)
+
+        last_noun_name = ""
+        first_noun_sequence_name = ""
+        first_noun_flag = 0
+        last_noun_sequence_name = ""
+        last_noun_flag = 1
+
+        for token in doc:
+            if token.pos in range(92, 101):
+                # Assume that general product name is last noun in title
+                last_noun_name = token.text
+
+                # Assume that general product name is the first sequence of just nouns
+                if first_noun_flag == 0:
+                    if first_noun_sequence_name != "":
+                        first_noun_sequence_name += " "
+                    first_noun_sequence_name += token.text
+
+                # Assume that general product name is the last sequence of just nouns
+                if last_noun_flag == 1:
+                    last_noun_sequence_name = ""
+                    last_noun_flag = 0
+                if last_noun_sequence_name != "":
+                    last_noun_sequence_name += " "
+                last_noun_sequence_name += token.text
+
+            else:
+                # Throw flag to terminate first noun sequence
+                if first_noun_sequence_name != "":
+                    first_noun_flag = 1
+                last_noun_flag = 1
+
+        first_noun_sequence_name.rstrip(" ")
         return \
             WdcProductType(
-                name=title.split()[-1]
+                name=last_noun_name,
+                confidence=1/3,
+                alternate=[first_noun_sequence_name, last_noun_sequence_name]
             )
--- mowgli_etl/pipeline/wdc/wdc_product_type.py
+++ mowgli_etl/pipeline/wdc/wdc_product_type.py
@@ -1,6 +1,7 @@
-from typing import NamedTuple, Optional
+from typing import NamedTuple, Optional, List
 
 
 class WdcProductType(NamedTuple):
     name: str
     confidence: Optional[float] = None
+    alternate: Optional[List[str]] = None
