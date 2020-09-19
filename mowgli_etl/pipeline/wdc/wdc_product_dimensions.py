2d9ad0bb43872f3b7b4b586cf6b7d8251a0539f2
Jesse E <ellinj2@rpi.edu>
2020-09-19 14:45:48 -0700

    Added dimension categories that need clarifying

--- mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
+++ mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
@@ -2,6 +2,11 @@ from typing import NamedTuple, Optional
 
 
 class WdcProductDimensions(NamedTuple):
+	"""
+	Should we handle volume, mass dimensions?
+	How do we handle multiple dimensions?
+	How do we handle clothing dimensions?
+	"""
     depth: Optional[float]
     height: Optional[float]
     length: Optional[float]
