2f4821d56bdb2a306f1dbb8e2b55083958d69c29
Jesse E <ellinj2@rpi.edu>
2020-09-19 17:18:08 -0700

    Added parameter for unit

--- mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
+++ mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
@@ -2,12 +2,13 @@ from typing import NamedTuple, Optional
 
 
 class WdcProductDimensions(NamedTuple):
-	"""
-	Should we handle volume, mass dimensions?
-	How do we handle multiple dimensions?
-	How do we handle clothing dimensions?
-	"""
+    """
+    Should we handle volume, mass dimensions?
+    How do we handle multiple dimensions?
+    How do we handle clothing dimensions?
+    """
     depth: Optional[float]
     height: Optional[float]
     length: Optional[float]
     width: Optional[float]
+    unit: Optional[str]
