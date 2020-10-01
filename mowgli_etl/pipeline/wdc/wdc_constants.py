eb907370111efebb4551120163ecb814a18c8253
Jesse E <ellinj2@rpi.edu>
2020-09-30 18:44:14 -0700

    Continued manipulation of re string

--- mowgli_etl/pipeline/wdc/wdc_constants.py
+++ mowgli_etl/pipeline/wdc/wdc_constants.py
@@ -6,4 +6,9 @@ WDC_DATASOURCE_ID = 'wdc'
 WDC_ARCHIVE_PATH = DATA_DIR / 'wdc' / 'extracted'
 WDC_CSV_FILE_KEY = 'wdc_csv_file'
 
+WDC_RE_DIMENSION_DECIMAL_STR = r"\d+\s\d+[hwl]\s"
+WDC_RE_DIMENSION_STR = r"\d+[hwl]\s"
+WDC_RE_DIMENSION_UNIT_STR = r"(?<=\d\w\s)\w\w"
+WDC_UNITS = ["ml", "cm", "mm", "in", "ft", "m"]
+
 WDC_HAS_DIMENSIONS = "has dimensions"
