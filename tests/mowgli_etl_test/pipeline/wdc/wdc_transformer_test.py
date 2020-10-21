1d5f5e26b9f4711a4ddb0844e47b923ebcc16fe1
Jesse E <ellinj2@rpi.edu>
2020-10-20 18:56:05 -0700

    Fixed naming conflict

--- tests/mowgli_etl_test/pipeline/wdc/wdc_transformer_test.py
+++ tests/mowgli_etl_test/pipeline/wdc/wdc_transformer_test.py
@@ -2,12 +2,12 @@ from pathlib import Path
 
 from mowgli_etl.model.kg_edge import KgEdge
 from mowgli_etl.pipeline.wdc.wdc_constants import WDC_DATASOURCE_ID
-from mowgli_etl.pipeline.wdc.wdc_transformer import WDCTransformer
+from mowgli_etl.pipeline.wdc.wdc_transformer import WdcTransformer
 
 
 def test_transform(wdc_jsonl_file_path: Path):
     edges = []
-    for edge in WDCTransformer().transform(wdc_jsonl_file_path=wdc_jsonl_file_path):
+    for edge in WdcTransformer().transform(wdc_jsonl_file_path=wdc_jsonl_file_path):
         assert isinstance(edge, KgEdge)
         assert edge.id
         assert edge.sources == (WDC_DATASOURCE_ID,)
