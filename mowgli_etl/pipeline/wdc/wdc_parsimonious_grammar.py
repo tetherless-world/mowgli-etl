d9144dabae68ec10e1a3ff4ddeee34519efe8d10
Jesse E <ellinj2@rpi.edu>
2020-10-27 13:00:33 -0700

    Black formatted

--- mowgli_etl/pipeline/wdc/wdc_parsimonious_grammar.py
+++ mowgli_etl/pipeline/wdc/wdc_parsimonious_grammar.py
@@ -8,10 +8,11 @@ from mowgli_etl.pipeline.wdc.wdc_constants import WDC_ARCHIVE_PATH
 
 from parsimonious import Grammar
 
+
 class WdcParsimoniousDimensionParser(WdcDimensionParser):
-	def __init__(self):
-		self.__GRAMMAR = Grammar(
-						"""
+    def __init__(self):
+        self.__GRAMMAR = Grammar(
+            """
 						bin 			= (space* word* number*)*
 
 						unit			= 'cm'/'in'/'ft'/'mm'/'m'
@@ -23,20 +24,22 @@ class WdcParsimoniousDimensionParser(WdcDimensionParser):
 						word 			= ~'[A-z]*'
 						space			= ~'\s'
 						"""
-						)
+        )
 
-	def parse(self, *, entry: WdcOffersCorpusEntry):
-		if entry.description is not None:
-			description = self.__GRAMMAR.parse(entry.description)
-			print(description)
+    def parse(self, *, entry: WdcOffersCorpusEntry):
+        if entry.description is not None:
+            description = self.__GRAMMAR.parse(entry.description)
+            print(description)
 
 
-if __name__=="__main__":
-	with open(
-	    WDC_ARCHIVE_PATH / "offers_corpus_english_v2_random_100_clean.jsonl", "r"
-	) as data:
-	    count = 0
-	    for row in data:
-	        count += 1
-	        if count == 34:
-	        	WdcParsimoniousDimensionParser().parse(entry=WdcOffersCorpusEntry.from_json(row))
+if __name__ == "__main__":
+    with open(
+        WDC_ARCHIVE_PATH / "offers_corpus_english_v2_random_100_clean.jsonl", "r"
+    ) as data:
+        count = 0
+        for row in data:
+            count += 1
+            if count == 34:
+                WdcParsimoniousDimensionParser().parse(
+                    entry=WdcOffersCorpusEntry.from_json(row)
+                )
