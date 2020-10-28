3d00023aa2fb050e2e0af0da9a838d78e2b25a7d
Jesse E <ellinj2@rpi.edu>
2020-10-28 14:14:53 -0700

    Changed to dataclass from dict, added reset() functionality

--- mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_node_visitor.py
+++ mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_node_visitor.py
@@ -1,9 +1,28 @@
 from parsimonious.nodes import NodeVisitor
 
+from dataclasses import dataclass, fields
+from typing import Optional
 
 class WdcParsimoniousNodeVisitor(NodeVisitor):
+
+    @dataclass(frozen=True)
+    class __Node:
+        @dataclass
+        class __Entry:
+            value: Optional[float] = None
+            unit: Optional[str] = None
+        width: Optional[__Entry] = __Entry()
+        depth: Optional[__Entry] = __Entry()
+        height: Optional[__Entry] = __Entry()
+        length: Optional[__Entry] = __Entry()
+
     def __init__(self):
-        self.dictionary = {}
+        self.dictionary = WdcParsimoniousNodeVisitor.__Node()
+
+    def reset(self):
+        for key in fields(self.dictionary):
+                getattr(self.dictionary, key.name).value = None
+                getattr(self.dictionary, key.name).unit = None
 
     def visit_dimension(self, node, visited_children):
         entries = node.text.split(" ")
@@ -17,10 +36,12 @@ class WdcParsimoniousNodeVisitor(NodeVisitor):
             key = "width"
         elif key == "h":
             key = "height"
-        self.dictionary[key] = float(value)
+        self.dictionary.__getattribute__(key).value = float(value)
 
     def visit_unit(self, node, visited_children):
-        self.dictionary["unit"] = node.text
+        value = node.text.split(" ")[-1]
+        for key in fields(self.dictionary):
+        	getattr(self.dictionary, key.name).unit = value
 
     def generic_visit(self, node, visited_children):
         return None
