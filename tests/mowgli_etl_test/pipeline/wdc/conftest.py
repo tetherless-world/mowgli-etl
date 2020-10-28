d2685e56182246fcf5097f04a5cc358250b13145
Jesse E <ellinj2@rpi.edu>
2020-10-28 14:32:20 -0700

    Black formatted

--- tests/mowgli_etl_test/pipeline/wdc/conftest.py
+++ tests/mowgli_etl_test/pipeline/wdc/conftest.py
@@ -5,8 +5,9 @@ from mowgli_etl.pipeline.wdc.wdc_constants import WDC_ARCHIVE_PATH
 
 @pytest.fixture
 def wdc_jsonl_file_path():
-    return WDC_ARCHIVE_PATH/"offers_corpus_english_v2_random_100.jsonl"
-    
+    return WDC_ARCHIVE_PATH / "offers_corpus_english_v2_random_100.jsonl"
+
+
 @pytest.fixture
 def wdc_json_clean_file_path():
-	return WDC_ARCHIVE_PATH/"offers_corpus_english_v2_random_100_clean.jsonl"
+    return WDC_ARCHIVE_PATH / "offers_corpus_english_v2_random_100_clean.jsonl"
