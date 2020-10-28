4dd2d59516d8ae760908acbdd200798f5f2748c0
Jesse E <ellinj2@rpi.edu>
2020-10-27 19:47:32 -0700

    Translate abbreviated dimensions to fit dimensions fields

--- mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_node_visitor.py
+++ mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_node_visitor.py
@@ -6,8 +6,18 @@ class WdcParsimoniousNodeVisitor(NodeVisitor):
         self.dictionary = {}
 
     def visit_dimension(self, node, visited_children):
-        value, key = node.text.split(" ")
-        self.dictionary[key] = value
+        entries = node.text.split(" ")
+        value = ".".join(entries[0:-1])
+        key = entries[-1]
+        if key=='l':
+        	key = 'length'
+        elif key=='d':
+        	key = 'depth'
+        elif key=='w':
+        	key = 'width'
+        elif key=='h':
+        	key = 'height'
+        self.dictionary[key] = float(value)
 
     def visit_unit(self, node, visited_children):
         self.dictionary["unit"] = node.text
