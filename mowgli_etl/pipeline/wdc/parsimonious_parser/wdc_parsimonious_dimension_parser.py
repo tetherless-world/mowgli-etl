fab9fd7c7dd3bd71407392b82f4815f5b06fd839
Jesse E <ellinj2@rpi.edu>
2020-10-27 19:48:15 -0700

    Manually clear the parse tree in case of multiple field parsing

--- mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_dimension_parser.py
+++ mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_dimension_parser.py
@@ -9,6 +9,8 @@ from mowgli_etl.pipeline.wdc.parsimonious_parser.wdc_parsimonious_node_visitor i
     WdcParsimoniousNodeVisitor,
 )
 
+import dataclasses
+
 from parsimonious import Grammar
 
 
@@ -16,12 +18,12 @@ class WdcParsimoniousDimensionParser(WdcDimensionParser):
     def __init__(self):
         self.__GRAMMAR = Grammar(
             """
-						bin 			= (space/unit/decimal/dimensions/dimension/direction/number/word)*
+						bin 			= (space/unit/dimensions/dimension/decimal/direction/number/word)*
 
 						unit			= dimension space ('cm'/'in'/'ft'/'mm'/'m')
-                        decimal         = number+ space number+
 						dimensions 		= (dimension space "x" space)+ dimension
-						dimension 		= (number+/decimal) space direction
+						dimension 		= (decimal/number) space direction
+                        decimal         = number+ space number+
 						direction 		= ("h"/"w"/"d"/"l") &(space/~'$')
 						number 			= ~'[0-9]+'
 						word 			= ~'[A-z]*'
@@ -30,12 +32,28 @@ class WdcParsimoniousDimensionParser(WdcDimensionParser):
         )
         self.__VISITOR = WdcParsimoniousNodeVisitor()
 
+    def __generate_dimensions(self):
+        retVal = WdcProductDimensions.from_dict(self.__VISITOR.dictionary)
+        self.__VISITOR.dictionary = {}
+        return retVal
+
     def parse(self, *, entry: WdcOffersCorpusEntry):
+        returns = []
+        return_flag = False
+        if entry.keyValuePairs is not None:
+            if "dimensions" in entry.keyValuePairs.keys():
+                keyValuePairs = self.__GRAMMAR.parse(entry.keyValuePairs["dimensions"])
+                self.__VISITOR.visit(keyValuePairs)
+                returns.append(
+                    (self.__generate_dimensions(), keyValuePairs)
+                )
+
         if entry.description is not None:
             description = self.__GRAMMAR.parse(entry.description)
             self.__VISITOR.visit(description)
-            if self.__VISITOR.dictionary.keys():
-                print(self.__VISITOR.dictionary)
+            returns.append((self.__generate_dimensions(), description))
+
+        return returns
 
 
 if __name__ == "__main__":
@@ -45,6 +63,19 @@ if __name__ == "__main__":
         count = 0
         for row in data:
             count += 1
-            WdcParsimoniousDimensionParser().parse(
+            dimension = WdcParsimoniousDimensionParser().parse(
                 entry=WdcOffersCorpusEntry.from_json(row)
             )
+            if dimension:
+                for d in dimension:
+                    output = ""
+                    spacing = "\t"
+                    data = d[0]
+                    for f in dataclasses.fields(data):
+                        if getattr(data, f.name) is not None:
+                            output += f"{spacing}{f.name}: {getattr(data,f.name)}\n"
+                    if output != "":
+                        print(f"NEW DIMENSION OBJECT from entry {count}\n{output}".strip())
+                        if len(sys.argv) >= 2 and sys.argv[1] == "origin":
+                            print("\nWhich was produced from:\n")
+                            print(d[1])
