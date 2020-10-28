8f5dbb3e59bd6bf7f339c0281de3eff109350606
Jesse E <ellinj2@rpi.edu>
2020-10-27 19:46:30 -0700

    Added default values to fix odd Parsimonious bug

--- mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
+++ mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
@@ -1,22 +1,26 @@
-from typing import NamedTuple, Optional
+from dataclasses import dataclass
+from typing import Optional
 
+from dataclasses_json import dataclass_json
 
-class WdcProductDimensions(NamedTuple):
+@dataclass_json
+@dataclass(frozen=True)
+class WdcProductDimensions:
     """
     Should we handle volume, mass dimensions?
     How do we handle multiple dimensions?
     How do we handle clothing dimensions?
     """
 
-    depth: Optional[float]
-    height: Optional[float]
-    length: Optional[float]
-    width: Optional[float]
-    volume: Optional[float]
-    mass: Optional[float]
-    depth_unit: Optional[str]
-    height_unit: Optional[str]
-    length_unit: Optional[str]
-    width_unit: Optional[str]
-    volume_unit: Optional[str]
-    mass_unit: Optional[str]
+    depth: Optional[float] = None
+    height: Optional[float] = None
+    length: Optional[float] = None
+    width: Optional[float] = None
+    volume: Optional[float] = None
+    mass: Optional[float] = None
+    depth_unit: Optional[str] = None
+    height_unit: Optional[str] = None
+    length_unit: Optional[str] = None
+    width_unit: Optional[str] = None
+    volume_unit: Optional[str] = None
+    mass_unit: Optional[str] = None
