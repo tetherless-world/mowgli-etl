0a46a104077a0227cfc1e776529699fd57285603
Jesse E <ellinj2@rpi.edu>
2020-10-28 19:16:56 -0700

    Fixed serialization issues

--- mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_node_visitor.py
+++ mowgli_etl/pipeline/wdc/parsimonious_parser/wdc_parsimonious_node_visitor.py
@@ -5,38 +5,36 @@ from typing import Optional
 
 
 class WdcParsimoniousNodeVisitor(NodeVisitor):
-    KeyMap = {"l": "length", "d": "depth", "w": "width", "h": "height"}
+    KEY_MAP = {"l": "length", "d": "depth", "w": "width", "h": "height"}
 
-    @dataclass(frozen=True)
+    @dataclass
     class __Node:
         @dataclass
-        class __Entry:
+        class Entry:
             value: Optional[float] = None
             unit: Optional[str] = None
 
-        width: Optional[__Entry] = __Entry()
-        depth: Optional[__Entry] = __Entry()
-        height: Optional[__Entry] = __Entry()
-        length: Optional[__Entry] = __Entry()
+        width: Optional[Entry] = None
+        depth: Optional[Entry] = None
+        height: Optional[Entry] = None
+        length: Optional[Entry] = None
 
     def __init__(self):
         self.dictionary = WdcParsimoniousNodeVisitor.__Node()
 
-    def reset(self):
-        for key in fields(self.dictionary):
-            getattr(self.dictionary, key.name).value = None
-            getattr(self.dictionary, key.name).unit = None
-
     def visit_dimension(self, node, visited_children):
         entries = node.text.split(" ")
         value = ".".join(entries[0:-1])
-        key = WdcParsimoniousNodeVisitor.KeyMap[entries[-1]]
-        self.dictionary.__getattribute__(key).value = float(value)
+        key = WdcParsimoniousNodeVisitor.KEY_MAP[entries[-1]]
+        if getattr(self.dictionary, key) is None:
+            setattr(self.dictionary, key, self.dictionary.Entry())
+        getattr(self.dictionary, key).value = float(value)
 
     def visit_unit(self, node, visited_children):
         value = node.text.split(" ")[-1]
         for key in fields(self.dictionary):
-            getattr(self.dictionary, key.name).unit = value
+            if getattr(self.dictionary, key.name) is not None:
+                getattr(self.dictionary, key.name).unit = value
 
     def generic_visit(self, node, visited_children):
         return None
