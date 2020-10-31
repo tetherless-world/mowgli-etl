990eeeccf65937be668b0ca41a1f0daf09680a3e
Jesse E <ellinj2@rpi.edu>
2020-10-31 13:43:12 -0700

    Forced float weight type

--- mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
+++ mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
@@ -7,8 +7,7 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass(frozen=True)
 class WdcProductDimensions:
-    """
-    """
+    """"""
 
     @dataclass
     class __Dimension:
@@ -25,12 +24,12 @@ class WdcProductDimensions:
     def __weight_accuracy(self, weight: float) -> float:
         if self.weight is None:
             return 0
-        return weight if self.weight.unit else weight/2
+        return weight if self.weight.unit else weight / 2
 
     def __power_accuracy(self, weight: float) -> float:
         if self.power is None:
             return 0
-        return weight if self.power.unit else weight/2
+        return weight if self.power.unit else weight / 2
 
     def accuracy(self, weight: float) -> float:
         tally = 0
