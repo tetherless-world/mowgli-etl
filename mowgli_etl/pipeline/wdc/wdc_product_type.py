22e428a2f73c17a7fc4d7cc7b2c014de9f15778d
Jesse E <ellinj2@rpi.edu>
2020-11-05 19:51:10 -0800

    Added source and method fields

--- mowgli_etl/pipeline/wdc/wdc_product_type.py
+++ mowgli_etl/pipeline/wdc/wdc_product_type.py
@@ -8,6 +8,18 @@ class WdcProductType:
 	class __Option:
 		name: str
 		confidence: float
+		method: str
 
-	expected_name: Optional[__Option] = None
-	possible_names: Optional[List[__Option]] = None
+	expected: Optional[__Option] = None
+	possible: Optional[List[__Option]] = None
+	source: str = None
+
+	def __init__(self, *, options, source):
+		self.possible = []
+		for n in options:
+			self.possible.append(n)
+		self.expected = sorted(self.possible, key=lambda x: (x.confidence, len(x.name)), reverse=True)[0]
+		self.source = source
+
+	def option(name, confidence, method):
+		return WdcProductType.__Option(name, confidence, method)
