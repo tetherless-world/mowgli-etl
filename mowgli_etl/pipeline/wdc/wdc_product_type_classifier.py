43d879ddcb3fb01654c632d79e2db96eb670e7fa
Jesse E <ellinj2@rpi.edu>
2020-11-07 14:01:03 -0800

    Refactored for dataclass structure

--- mowgli_etl/pipeline/wdc/wdc_product_type_classifier.py
+++ mowgli_etl/pipeline/wdc/wdc_product_type_classifier.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
 from mowgli_etl.pipeline.wdc.wdc_product_type import WdcProductType
-
+from mowgli_etl.pipeline.wdc.wdc_offers_corpus_entry import WdcOffersCorpusEntry
 
 class WdcProductTypeClassifier(ABC):
     @abstractmethod
-    def classify(self, *, title: str) -> WdcProductType:
+    def classify(self, *, entry: WdcOffersCorpusEntry) -> WdcProductType:
         """
         Parse title/listing/other to pull ProductType with confidence value
         """
