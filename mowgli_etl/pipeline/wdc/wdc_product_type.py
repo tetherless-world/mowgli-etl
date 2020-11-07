1ea974831eabd27bccddbe423bff48c78a1c02ff
Jesse E <ellinj2@rpi.edu>
2020-11-07 14:00:37 -0800

    Simplified to a single __init__ function

--- mowgli_etl/pipeline/wdc/wdc_product_type.py
+++ mowgli_etl/pipeline/wdc/wdc_product_type.py
@@ -2,10 +2,10 @@ from typing import Optional, List
 from dataclasses import dataclass
 from collections import Counter
 
-@dataclass
+@dataclass(init=False)
 class WdcProductType:
 
-	METHOD_CONFIDENCE = {"category": 1/2, "description": 1/10, "title": 1/4, "specTableContent": 1/3}
+	METHOD_CONFIDENCE = {"category": 1/2, "description": 1/10, "title": 1/4, "spec_table_content": 1/3}
 
 	@dataclass
 	class __Option:
@@ -19,20 +19,15 @@ class WdcProductType:
 	key_confidence: float = None
 	source: str = None
 
-	def __init__(self, *, options, source):
+	def __init__(self, *, options, source, key):
 		self.possible = []
 		name_counter = Counter()
 		for n in options:
-			self.possible.append(n)
-			name_counter[n.name] += 1
-		for n in options:
+			self.possible.append(WdcProductType.__Option(n[0],n[1],n[2]))
+			name_counter[n[0]] += 1
+		for n in self.possible:
 			n.confidence *= name_counter[n.name]
 		self.expected = sorted(self.possible, key=lambda x: (x.confidence, len(x.name)), reverse=True)[0]
 		self.source = source
-
-	def option(name, confidence, method):
-		return WdcProductType.__Option(name, confidence, method)
-
-	def set_key(self, key):
 		self.key = key
 		self.key_confidence = WdcProductType.METHOD_CONFIDENCE[key]
