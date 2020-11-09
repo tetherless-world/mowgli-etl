bf486e8f86f5e36beb182a015e993bc735afa2c9
Jesse E <ellinj2@rpi.edu>
2020-11-09 11:50:43 -0800

    Black formatted

--- tests/mowgli_etl_test/pipeline/wdc/wdc_heuristic_product_type_classifier_test.py
+++ tests/mowgli_etl_test/pipeline/wdc/wdc_heuristic_product_type_classifier_test.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from json import loads
 
 from mowgli_etl.pipeline.wdc.wdc_heuristic_product_type_classifier import (
-    WdcHeuristicProductTypeClassifier
+    WdcHeuristicProductTypeClassifier,
 )
 from mowgli_etl.pipeline.wdc.wdc_offers_corpus_entry import WdcOffersCorpusEntry
 
@@ -11,36 +11,37 @@ def test_heuristic_product_type_classifier_clean(wdc_json_clean_file_path: Path)
     with open(wdc_json_clean_file_path) as data:
         HPTC = WdcHeuristicProductTypeClassifier()
         for count, row in enumerate(data):
-            if count+1 == 2:
+            if count + 1 == 2:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Clothing"
                 assert f"{item.expected.confidence:.3%}" == "100.000%"
-            elif count+1 in [3,4]:
+            elif count + 1 in [3, 4]:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Sports"
                 assert f"{item.expected.confidence:.3%}" == "100.000%"
-            elif count+1 == 5:
+            elif count + 1 == 5:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Office Products"
                 assert f"{item.expected.confidence:.3%}" == "66.667%"
+
 
 def test_heuristic_product_type_classifier_large(wdc_large_json_file_path: Path):
     with open(wdc_large_json_file_path) as data:
         HPTC = WdcHeuristicProductTypeClassifier()
         for count, row in enumerate(data):
-            if count+1 == 1:
+            if count + 1 == 1:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Electronics"
                 assert f"{item.expected.confidence:.3%}" == "100.000%"
-            elif count+1 == 2:
+            elif count + 1 == 2:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Jewelry"
                 assert f"{item.expected.confidence:.3%}" == "100.000%"
-            elif count+1 == 3:
+            elif count + 1 == 3:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Gourmet Food"
                 assert f"{item.expected.confidence:.3%}" == "33.333%"
-            elif count+1 == 5:
+            elif count + 1 == 5:
                 item = next(HPTC.classify(entry=WdcOffersCorpusEntry.from_json(row)))
                 assert item.expected.name == "Photo"
                 assert f"{item.expected.confidence:.3%}" == "66.667%"
--- tests/mowgli_etl_test/pipeline/wdc/wdc_parsimonious_dimension_parser_test.py
+++ tests/mowgli_etl_test/pipeline/wdc/wdc_parsimonious_dimension_parser_test.py
@@ -12,7 +12,7 @@ SOURCE_KEY = {"description": 1 / 2, "spec_table_content": 3 / 4, "key_value_pair
 
 def test_parsimonious_parser(wdc_json_clean_file_path: Path):
     with open(wdc_json_clean_file_path, "r") as data:
-        for count,entry in enumerate(data):
+        for count, entry in enumerate(data):
             count += 1
             entry = WdcOffersCorpusEntry.from_json(entry)
             if count == 12:
@@ -33,31 +33,42 @@ def test_parsimonious_parser(wdc_json_clean_file_path: Path):
                 assert dimensions[0].dimensions.height.unit == None
                 assert dimensions[0].dimensions.width.value == 72.0
                 assert dimensions[0].dimensions.depth.value == 32.0
-                assert dimensions[0].dimensions.accuracy(SOURCE_KEY[dimensions[0].field]) == 0.5
+                assert (
+                    dimensions[0].dimensions.accuracy(SOURCE_KEY[dimensions[0].field])
+                    == 0.5
+                )
 
                 assert dimensions[1].dimensions.height.value == 36.0
                 assert dimensions[1].dimensions.height.unit == None
                 assert dimensions[1].dimensions.length.value == 72.0
                 assert dimensions[1].dimensions.width.value == 32.0
-                assert dimensions[1].dimensions.accuracy(SOURCE_KEY[dimensions[1].field]) == 0.25
+                assert (
+                    dimensions[1].dimensions.accuracy(SOURCE_KEY[dimensions[1].field])
+                    == 0.25
+                )
 
                 assert dimensions[2].dimensions.depth.value == 32.0
                 assert dimensions[2].dimensions.depth.unit == None
                 assert dimensions[2].dimensions.height.value == 36.0
                 assert dimensions[2].dimensions.width.value == 72.0
-                assert dimensions[2].dimensions.accuracy(SOURCE_KEY[dimensions[2].field]) == 0.375
+                assert (
+                    dimensions[2].dimensions.accuracy(SOURCE_KEY[dimensions[2].field])
+                    == 0.375
+                )
 
 
 def test_parsimonious_parser_large(wdc_large_json_file_path: Path):
     with open(wdc_large_json_file_path, "r") as data:
-        for count,entry in enumerate(data):
+        for count, entry in enumerate(data):
             count += 1
             entry = WdcOffersCorpusEntry.from_json(entry)
             if count == 24:
                 dimension = WdcParsimoniousDimensionParser().parse(entry=entry)[0]
                 assert dimension.dimensions.weight.value == 1.0
                 assert dimension.dimensions.weight.unit == "g"
-                assert dimension.dimensions.accuracy(SOURCE_KEY[dimension.field]) == 0.75
+                assert (
+                    dimension.dimensions.accuracy(SOURCE_KEY[dimension.field]) == 0.75
+                )
 
             elif count == 88:
                 dimension = WdcParsimoniousDimensionParser().parse(entry=entry)[0]
@@ -69,8 +80,14 @@ def test_parsimonious_parser_large(wdc_large_json_file_path: Path):
                 dimensions = WdcParsimoniousDimensionParser().parse(entry=entry)
                 assert dimensions[0].dimensions.weight.value == 10.92
                 assert dimensions[0].dimensions.weight.unit == "lbs"
-                assert dimensions[0].dimensions.accuracy(SOURCE_KEY[dimensions[0].field]) == 1
+                assert (
+                    dimensions[0].dimensions.accuracy(SOURCE_KEY[dimensions[0].field])
+                    == 1
+                )
 
                 assert dimensions[1].dimensions.weight.value == 368.0
                 assert dimensions[1].dimensions.weight.unit == "g"
-                assert dimensions[1].dimensions.accuracy(SOURCE_KEY[dimensions[1].field]) == 0.75
+                assert (
+                    dimensions[1].dimensions.accuracy(SOURCE_KEY[dimensions[1].field])
+                    == 0.75
+                )
