06fef5538ec5ea0fe5a195fea078bf60bbcb990c
Jesse E <ellinj2@rpi.edu>
2020-11-09 19:24:55 -0800

    Refactored random sample to return WdcOfferCorpusEntry objects

--- mowgli_etl/pipeline/wdc/Data_Exploration/offers_corpus_english_v2_random_sample.py
+++ mowgli_etl/pipeline/wdc/Data_Exploration/offers_corpus_english_v2_random_sample.py
@@ -1,14 +1,18 @@
-import random
+from random import sample
+from pathlib import Path
 
-from mowgli_etl.paths import DATA_DIR
+from mowgli_etl.pipeline.wdc.wdc_offers_corpus_entry import WdcOffersCorpusEntry
 
-with open(
-    DATA_DIR / "wdc" / "extracted" / "offers_corpus_english_v2_1000.jsonl"
-) as file:
-    lines = random.sample(file.readlines(), 100)
-
-with open(
-    DATA_DIR / "wdc" / "extracted" / "offers_corpus_english_v2_random_100.jsonl", "w"
-) as file:
-    for line in lines:
-        file.write(line)
+def sample(wdc_jsonl_file_path: Path, n: int) -> WdcOffersCorpusEntry:
+	if n < 1:
+		print(f"ERROR: Desired number of lines {n} is not a positive integer")
+		return None
+	with open(wdc_jsonl_file_path) as data:
+		line_count = data.read().count('\n') + 1
+		if n > line_count:
+			print(f"ERROR: Desired number of lines {n} is beyond the length of the file {line_count}")
+			return None
+		lines = random.sample(1, line_count, n)
+		for i, row in enumerate(data):
+			if i in lines:
+				yield WdcOffersCorpusEntry.from_json(row)
