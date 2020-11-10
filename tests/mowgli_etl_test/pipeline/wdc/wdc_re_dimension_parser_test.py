f79648e0e499686f5e65d42557cc88afde47c5f7
Jesse E <ellinj2@rpi.edu>
2020-11-10 13:28:11 -0800

    Regex isn't viable. Relying on Parsimonious instead

--- tests/mowgli_etl_test/pipeline/wdc/wdc_re_dimension_parser_test.py
+++
@@ -1,8 +0,0 @@
-from mowgli_etl.pipeline.wdc.wdc_re_dimension_parser import (
-    WdcREDimensionParser as WdcReDP,
-)
-from mowgli_etl.pipeline.wdc.wdc_offers_corpus import WdcOffersCorpus
-
-# Needs to be implemented with actual tests
-def test_re_dimension_parser(wdc_large_offers_corpus: WdcOffersCorpus):
-    pass
