bc880d0011861e8689be69fe8040ec37ce6c3635
Jesse E <ellinj2@rpi.edu>
2020-11-10 13:13:29 -0800

    Swapped json_file Path for OfferCorpus object

--- tests/mowgli_etl_test/pipeline/wdc/conftest.py
+++ tests/mowgli_etl_test/pipeline/wdc/conftest.py
@@ -1,7 +1,8 @@
 import pytest
 
 from mowgli_etl.pipeline.wdc.wdc_constants import WDC_ARCHIVE_PATH
+from mowgli_etl.pipeline.wdc.wdc_offers_corpus import WdcOffersCorpus
 
 @pytest.fixture
-def wdc_large_json_file_path():
-    return WDC_ARCHIVE_PATH / "offers_corpus_english_v2_1000.jsonl"
+def wdc_large_offers_corpus():
+	return WdcOffersCorpus(wdc_json_file_path = WDC_ARCHIVE_PATH / "offers_corpus_english_v2_1000.jsonl")
