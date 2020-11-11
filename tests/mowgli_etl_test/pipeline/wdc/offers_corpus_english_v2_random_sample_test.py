4a305020fe37c75dd989ee3ae9a7d0826baf85f8
Jesse E <ellinj2@rpi.edu>
2020-11-10 16:54:40 -0800

    Switched to pytest.raises(ValueError)

--- tests/mowgli_etl_test/pipeline/wdc/offers_corpus_english_v2_random_sample_test.py
+++ tests/mowgli_etl_test/pipeline/wdc/offers_corpus_english_v2_random_sample_test.py
@@ -1,9 +1,6 @@
 from mowgli_etl.pipeline.wdc.wdc_offers_corpus import WdcOffersCorpus
 from pathlib import Path
-
-import warnings
-
-warnings.simplefilter(action="ignore", category=StopIteration)
+import pytest
 
 
 def test_100_data_sample(wdc_large_offers_corpus: WdcOffersCorpus):
@@ -14,10 +11,12 @@ def test_100_data_sample(wdc_large_offers_corpus: WdcOffersCorpus):
 
 
 def test_small_data_sample(wdc_large_offers_corpus: WdcOffersCorpus):
-    for item in wdc_large_offers_corpus.sample(0):
-        assert item is None
+    with pytest.raises(ValueError):
+	    for item in wdc_large_offers_corpus.sample(0):
+	        assert item is None
 
 
 def test_massive_data_sample(wdc_large_offers_corpus: WdcOffersCorpus):
-    for item in wdc_large_offers_corpus.sample(10 ** 8):
-        assert item is None
+	with pytest.raises(ValueError):
+	    for item in wdc_large_offers_corpus.sample(10 ** 8):
+	        assert item is None
