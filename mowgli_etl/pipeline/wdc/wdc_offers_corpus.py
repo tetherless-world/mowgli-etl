2d4e20c937fc3c84ef3df48704228592b49c6ef7
Jesse E <ellinj2@rpi.edu>
2020-11-10 16:36:37 -0800

    Refactored to standard class and entries() Generator

--- mowgli_etl/pipeline/wdc/wdc_offers_corpus.py
+++ mowgli_etl/pipeline/wdc/wdc_offers_corpus.py
@@ -1,26 +1,25 @@
-from dataclasses import dataclass
-from typing import Optional, List
+from typing import Optional, List, Generator
 from pathlib import Path
 from mowgli_etl.pipeline.wdc.wdc_offers_corpus_entry import WdcOffersCorpusEntry
 
 
-@dataclass(init=False)
 class WdcOffersCorpus:
-	entries: Optional[List[WdcOffersCorpusEntry]]
-
 	def __init__(self, *, wdc_json_file_path: Path):
-		self.entries = []
-		for row in open(wdc_json_file_path):
-			self.entries.append(WdcOffersCorpusEntry.from_json(row))
+		self.__file_path = wdc_json_file_path
+		self.__file_length = open(self.__file_path).read().count('\n') + 1
+
+	def entries(self) -> Generator[WdcOffersCorpusEntry, None, None]:
+		with open(self.__file_path) as data:
+			for row in data:
+				yield WdcOffersCorpusEntry.from_json(row)
 
 	def sample(self, n: int):
 		if n < 1:
-			print(f"ERROR: Desired number of lines {n} is not a positive integer")
-			return None
-		if n > len(self.entries):
-			print(f"ERROR: Desired number of lines {n} is beyond the number of entries {len(self.entries)}")
-			return None
-		step_size = len(self.entries)//n
-		for i in range(0, len(self.entries), step_size):
-			yield self.entries[i]
+			raise ValueError(f"ERROR: Desired number of lines {n} is not a positive integer")
+		if n > self.__file_length:
+			raise ValueError(f"ERROR: Desired number of lines {n} is beyond the number of entries {self.__file_length}")
+		step_size = self.__file_length//n
+		for i, entry in enumerate(self.entries()):
+			if i+1 % step_size == 0:
+				yield entry
 		return None
