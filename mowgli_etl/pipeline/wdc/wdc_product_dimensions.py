de0afc7f9e2dceffa1c6ad473770bb1ef71b6cce
Jesse E <ellinj2@rpi.edu>
2020-11-19 20:02:35 -0800

    Added documentation for Product Dimensions

--- mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
+++ mowgli_etl/pipeline/wdc/wdc_product_dimensions.py
@@ -7,10 +7,16 @@ from dataclasses_json import dataclass_json
 @dataclass_json
 @dataclass(frozen=True)
 class WdcProductDimensions:
-    """"""
+    """
+    Store product dimensions in a dataclass for easier access
+    """
 
     @dataclass
     class __Dimension:
+        """
+        Store dimensions separately to encapsulate information for each dimension
+        """
+
         value: Optional[float] = None
         unit: Optional[str] = None
         value_text: Optional[str] = None
@@ -24,16 +30,30 @@ class WdcProductDimensions:
     weight: Optional[__Dimension] = None
 
     def __weight_accuracy(self, weight: float) -> float:
+        """
+        Calculate weight accuracy dependent on whether or not there's a unit associated
+        """
+
         if self.weight is None:
             return 0
         return weight if self.weight.unit else weight / 2
 
     def __power_accuracy(self, weight: float) -> float:
+        """
+        Calculate power accuracy dependent on whether or not there's a unit associated
+        """
+
         if self.power is None:
             return 0
         return weight if self.power.unit else weight / 2
 
     def accuracy(self, weight: float) -> float:
+        """
+        Calculate overall accuracy of the parsed dimension
+        Parameters: weight - float associated to the source certainty
+        Return: float - certainty of dimension
+        """
+
         tally = 0
         for a in ("depth", "height", "length", "width"):
             if getattr(self, a) is not None:
