a2ac833f77042eb6af623f04d277f3781b689cb3
Jesse E <ellinj2@rpi.edu>
2020-11-19 19:56:19 -0800

    Added documentation to Offers Corpus

--- mowgli_etl/pipeline/wdc/wdc_offers_corpus.py
+++ mowgli_etl/pipeline/wdc/wdc_offers_corpus.py
@@ -5,15 +5,31 @@ from mowgli_etl.pipeline.wdc.wdc_offers_corpus_entry import WdcOffersCorpusEntry
 
 class WdcOffersCorpus:
     def __init__(self, *, wdc_json_file_path: Path):
+        """
+        Establish collection of product entries in dataclass enclosures
+        Parameters: wdc_json_file_path - Path object to data source
+        """
+
         self.__file_path = wdc_json_file_path
         self.__file_length = open(self.__file_path).read().count("\n") + 1
 
     def entries(self) -> Generator[WdcOffersCorpusEntry, None, None]:
+        """
+        Access entries from corpus
+        Yield: WdcOffersCorpusEntry - entry generated from each line in the data source
+        """
+
         with open(self.__file_path) as data:
             for row in data:
                 yield WdcOffersCorpusEntry.from_json(row)
 
-    def sample(self, n: int):
+    def sample(self, n: int) -> Generator[WdcOffersCorpusEntry, None, None]:
+        """
+        Randomly sample entries from the corpus to simplify testing and exploration
+        Parameters: n - int number of entries desired such that 0 < n < file_length
+        Yields: WdcOffersCorpusEntry - entry generated from randomly selected lines 
+        """
+
         if n < 1:
             raise ValueError(
                 f"ERROR: Desired number of lines {n} is not a positive integer"
@@ -26,4 +42,3 @@ class WdcOffersCorpus:
         for i, entry in enumerate(self.entries()):
             if (i + 1) % step_size == 0:
                 yield entry
-        return None
