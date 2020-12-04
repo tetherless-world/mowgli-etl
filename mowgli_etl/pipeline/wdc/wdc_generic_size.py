0d4dd0e9c713df70110aa07e89d80a3bc47176a5
Jesse E <ellinj2@rpi.edu>
2020-12-03 16:44:52 -0800

    Added attributes for averaging

--- mowgli_etl/pipeline/wdc/wdc_generic_size.py
+++ mowgli_etl/pipeline/wdc/wdc_generic_size.py
@@ -1,11 +1,14 @@
 from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 from typing import Optional
 from mowgli_etl.pipeline.wdc.wdc_product_dimensions import WdcProductDimensions
 
 
+@dataclass_json
 @dataclass
 class WdcGenericSize:
-	name: str
-	dimension: WdcProductDimensions
-	unit: str
-	bucket: Optional[int] = None
+    name: str
+    dimension: WdcProductDimensions
+    count: int
+    unit: str
+    bucket: Optional[int] = None
