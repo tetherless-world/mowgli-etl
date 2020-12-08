a1f880c2930f2d7a7ccf54ba2238f7202b2c8623
Jesse E <ellinj2@rpi.edu>
2020-12-08 10:46:49 -0800

    Added documentation and cleaned existing code

--- mowgli_etl/pipeline/wdc/wdc_size_buckets.py
+++ mowgli_etl/pipeline/wdc/wdc_size_buckets.py
@@ -1,7 +1,7 @@
 from typing import Tuple
 from mowgli_etl.pipeline.wdc.wdc_product_type import WdcProductType
 from mowgli_etl.pipeline.wdc.wdc_product_dimensions import WdcProductDimensions
-from mowgli_etl.pipeline.wdc.wdc_generic_size import WdcGenericSize
+from mowgli_etl.pipeline.wdc.wdc_generic_size import WdcProductSize
 
 
 class WdcSizeBuckets:
@@ -12,138 +12,47 @@ class WdcSizeBuckets:
         # Determine heuristics for buckets
         return None
 
-    def __convert(self, dimension, origin):
-        if origin in (
-            "in",
-            "lb",
-            "lbs",
-            "v",
-        ):
-            return dimension
-        if origin == "mm":
-            return dimension * 3.9
-        if origin == "cm":
-            return dimension * 0.39
-        if origin == "m":
-            return dimension * 0.0039
-        if origin == "ft":
-            return dimension * 12
-        if origin == "mv":
-            return dimension / 1000
-        if origin == "kv":
-            return dimension * 1000
-        if origin == "oz":
-            return dimension / 16
-        if origin == "g":
-            return dimension * 0.0022046
-        if origin == "mg":
-            return dimension * 0.0000022046
-        if origin == "kg":
-            return dimension * 2.2046
+    def generalize(self, wdc_product_type: WdcProductType, wdc_product_dimensions: WdcProductDimensions):
+        """
+		:param wdc_product_type: Parsed general product type
+		:type wdc_product_type: class<WdcProductType>
+		:param wdc_product_dimensions: Parsed product dimensions
+		:type wdc_product_dimensions: class<WdcProductDimensions>
+		:return: Generalized dimensions, not the average dimensions though
+		:rtype: class<WdcProductSize>
+        """
 
-    def __update(self, generic):
-        if generic.name not in self.averages:
-            return generic
-        new_object = {
-            "depth": None,
-            "height": None,
-            "length": None,
-            "width": None,
-            "power": None,
-            "weight": None,
-        }
-        local = self.averages[generic.name]
-        for a in ("depth", "height", "length", "width", "power", "weight"):
-            if getattr(generic.dimension, a) is not None:
-                new_object[a] = {"value": None, "unit": None}
-                new_object[a]["value"] = (
-                    getattr(generic.dimension, a).value
-                    + getattr(local.dimension, a).value * local.count
-                ) / (local.count + 1)
-                new_object[a]["unit"] = getattr(generic.dimension, a).unit
+        if not wdc_product_type or not wdc_product_dimensions:
+        	return None
 
-        updated_generic = {
-            "name": generic.name,
-            "dimension": new_object,
-            "count": local.count + 1,
-            "unit": generic.unit,
-        }
+        generalized_dimensions = wdc_product_dimensions.to_english()
 
-        return WdcGenericSize.from_dict(updated_generic)
-
-    def generalize(self, wdc_product: Tuple[WdcProductType, WdcProductDimensions]):
-        # General units: in^3, V, lb
-        product_type = wdc_product[0]
-        product_dimensions = wdc_product[1]
-        dimension = None
         unit = None
-        if (
-            product_dimensions.depth
-            or product_dimensions.height
-            or product_dimensions.length
-            or product_dimensions.width
-        ):
-            dimension = dict()
-            if product_dimensions.depth:
-                dimension["depth"] = {
-                    "value": self.__convert(
-                        product_dimensions.depth.value, product_dimensions.depth.unit
-                    ),
-                    "unit": "in",
-                }
-            if product_dimensions.height:
-                dimension["height"] = {
-                    "value": self.__convert(
-                        product_dimensions.height.value, product_dimensions.height.unit
-                    ),
-                    "unit": "in",
-                }
-            if product_dimensions.length:
-                dimension["length"] = {
-                    "value": self.__convert(
-                        product_dimensions.length.value, product_dimensions.height.unit
-                    ),
-                    "unit": "in",
-                }
-            if product_dimensions.width:
-                dimension["width"] = {
-                    "value": self.__convert(
-                        product_dimensions.width.value, product_dimensions.width.unit
-                    ),
-                    "unit": "in",
-                }
-            unit = "in^3"
-        elif product_dimensions.power:
-            dimension = {
-                "value": self.__convert(
-                    product_dimensions.power.value, product_dimensions.power.unit
-                ),
-                "unit": "V",
-            }
-            unit = "v'"
-        elif product_dimensions.weight:
-            dimension = {
-                "value": self.__convert(
-                    product_dimensions.weight.value, product_dimensions.weight.unit
-                ),
-                "unit": "lb",
-            }
-            unit = "lb"
 
-        if not dimension:
-            return None
-        generic_dimensions = self.__update(
-            WdcGenericSize(
-                {
-                    "name": product_type.expected.name,
-                    "dimension": WdcProductDimensions.from_dict(dimension),
+        for field in generalized_dimensions.keys():
+        	origin = getattr(generalized_dimensions, field.name)
+        	if origin and origin.unit:
+        		unit = origin.unit
+
+        generic_dimensions = WdcProductSize({
+                    "name": wdc_product_type.expected.name,
+                    "dimension": generalized_dimensions,
                     "count": 1,
                     "unit": unit,
-                }
-            )
-        )
+                })
 
-        generic_dimensions.bucket = self.__bucket(generic_dimensions)
-        self.averages[product_type.expected.name] = generic_dimensions
+        if generic_dimensions.name in self.averages:
+        	tracked_dimension = self.averages[generic_dimensions.name]
+        	for field in tracked_dimension.dimension.keys():
+        		dimension = getattr(tracked_dimension, field.name)
+        		if not dimension:
+        			continue
+        		dimension.value = (dimension.value * tracked_dimension.count + getattr(generic_dimensions, field.name).value) / (tracked_dimension.count + 1)
+        	tracked_dimension.count += 1
+
+        else:
+        	self.averages[generic_dimensions.name] = generic_dimensions
+
+        self.averages[generic_dimensions.name].bucket = self.__bucket(self.averages[generic_dimensions.name])
 
         return generic_dimensions
