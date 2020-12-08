45ace5b51ac661a66720ab9a72b784124932ce46
Jesse E <ellinj2@rpi.edu>
2020-12-08 10:45:55 -0800

    Changed name to WdcProductSize, added documentation

--- mowgli_etl/pipeline/wdc/wdc_generic_size.py
+++ mowgli_etl/pipeline/wdc/wdc_generic_size.py
@@ -6,7 +6,20 @@ from mowgli_etl.pipeline.wdc.wdc_product_dimensions import WdcProductDimensions
 
 @dataclass_json
 @dataclass
-class WdcGenericSize:
+class WdcProductSize:
+	"""
+	:field name: Product type name pulled from WdcProductType.expected
+	:type name: str
+	:field dimension: Generic product dimensions
+	:type dimension: class<WdcProductDimensions>
+	:field count: Number of WdcProducts used to calculate dimension. Used for updating the generic size if a new product is found
+	:type count: int
+	:field unit: Unit of measurement for the dimension. Separated from dimension for parsing purposes
+	:type unit: str
+	:field bucket: The size bucket the product fits in. Defautl=None
+	:tyep bucket: int
+	"""
+
     name: str
     dimension: WdcProductDimensions
     count: int
