96cdef6c21c51517e9f84e455800e2c4e2713ef8
Jesse E <ellinj2@rpi.edu>
2020-12-14 20:36:50 -0800

    Removed unreliable case

--- tests/mowgli_etl_test/pipeline/wdc/wdc_parsimonious_dimension_parser_test.py
+++ tests/mowgli_etl_test/pipeline/wdc/wdc_parsimonious_dimension_parser_test.py
@@ -27,20 +27,3 @@ def test_parsimonious_parser_large(wdc_large_offers_corpus: WdcOffersCorpus):
             assert dimension.dimensions.weight.value == 1.7
             assert dimension.dimensions.weight.unit == "oz"
             assert dimension.dimensions.accuracy(SOURCE_KEY[dimension.field]) == 0.5
-
-        elif count in (179,180):
-            dimensions = WdcParsimoniousDimensionParser().parse(entry=entry)
-            if not dimensions:
-                continue
-            assert dimensions[0].dimensions.weight.value == 10.92
-            assert dimensions[0].dimensions.weight.unit == "lbs"
-            assert (
-                dimensions[0].dimensions.accuracy(SOURCE_KEY[dimensions[0].field]) == 1
-            )
-
-            assert dimensions[1].dimensions.weight.value == 368.0
-            assert dimensions[1].dimensions.weight.unit == "g"
-            assert (
-                dimensions[1].dimensions.accuracy(SOURCE_KEY[dimensions[1].field])
-                == 0.75
-            )
