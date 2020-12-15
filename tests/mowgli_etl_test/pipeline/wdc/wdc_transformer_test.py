5e609a64e049e91c1d50021b71a559ea69c56ca5
Jesse E <ellinj2@rpi.edu>
2020-12-15 11:00:39 -0800

    Updated to work with current Transformer yields

--- tests/mowgli_etl_test/pipeline/wdc/wdc_transformer_test.py
+++ tests/mowgli_etl_test/pipeline/wdc/wdc_transformer_test.py
@@ -12,5 +12,19 @@ from mowgli_etl.pipeline.wdc.parsimonious_parser.wdc_parsimonious_dimension_pars
 from mowgli_etl.pipeline.wdc.wdc_offers_corpus import WdcOffersCorpus
 
 # Need to write actual tests
-def test_transform(wdc_large_offers_corpus: WdcOffersCorpus):
-    pass
+def test_wdc_transform(wdc_large_offers_corpus: WdcOffersCorpus):
+    transformer = WdcTransformer()
+    edges = []
+    node_ids = set()
+    predicates = set()
+    for result in transformer.transform(corpus=wdc_large_offers_corpus):
+        assert isinstance(result, KgEdge)
+        edges.append(result)
+        node_ids.add(result.object)
+        node_ids.add(result.subject)
+        predicates.add(result.predicate)
+
+    for edge in edges:
+        assert edge.subject in node_ids
+        assert edge.object in node_ids
+        assert edge.predicate in predicates
